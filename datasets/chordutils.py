f57c0e65feaa0a4fc3e5718f3a6b799115d962b2
Eric J. Humphrey <ejhumphrey@nyu.edu>
2013-09-27 19:16:00 -0400

    Added unique counters and fixed a bug with stripping carriage returns.

--- datasets/chordutils.py
+++ datasets/chordutils.py
@@ -2,6 +2,7 @@
 """
 
 import numpy as np
+from collections import OrderedDict
 
 NO_CHORD = "N"
 
@@ -25,14 +26,20 @@ def load_labfile(lab_file):
     boundaries = []
     labels = []
     for i, line in enumerate(open(lab_file)):
-        line = line.strip("\n")
+        line = line.strip("\n").strip("\r")
         if not line:
             # Assume we're done?
             break
-        line_parts = line.split("\t")
+        if "\t" in line:
+            line_parts = line.split("\t")
+        elif " " in line:
+            line_parts = []
+            for p in line.split(" "):
+                if len(p):
+                    line_parts.append(p)
         if len(line_parts) != 3:
             raise ValueError(
-                "Error parsing %s on line %d: %s" % (lab_file, i, line))
+                "Error parsing %s on line %d: %s" % (lab_file, i, repr(line)))
         start_time, end_time = float(line_parts[0]), float(line_parts[1])
         boundaries.append(start_time)
         labels.append(line_parts[-1])
@@ -52,8 +59,8 @@ def assign_labels_to_time_points(time_points, boundaries, labels):
     boundaries : np.ndarray
         Time boundaries between labels.
     labels : array_like
-        Chord labels corresponding to the interval between adjacent boundaries. 
-        
+        Chord labels corresponding to the interval between adjacent boundaries.
+
     Returns
     -------
     output_labels : list
@@ -63,6 +70,31 @@ def assign_labels_to_time_points(time_points, boundaries, labels):
     for t in time_points:
         if t < boundaries.min() or t > boundaries.max():
             output_labels.append(NO_CHORD)
-        index = np.argmax(boundaries > t) - 1
-        output_labels.append(labels[index])
+        else:
+            index = np.argmax(boundaries > t) - 1
+            output_labels.append(labels[index])
     return output_labels
+
+
+def collect_unique_labels(lab_files):
+    unique_labels = set()
+    for lab_file in lab_files:
+        unique_labels.update(load_labfile(lab_file)[1])
+    return unique_labels
+
+
+def count_chords(lab_files):
+    all_chords = dict()
+    for lab_file in lab_files:
+        boundaries, labels = load_labfile(lab_file)
+        for n, chord_label in enumerate(labels):
+            if not chord_label in all_chords:
+                all_chords[chord_label] = 0.0
+            all_chords[chord_label] += (boundaries[n + 1] - boundaries[n])
+    keys, values = [], []
+    [(keys.append(k), values.append(v)) for k, v in all_chords.iteritems()]
+    sorted_index = np.array(values).argsort()[::-1]
+    return OrderedDict([(keys[i], values[i]) for i in sorted_index])
+
+
+    return unique_labels
