4d5f08738408344b29f8286cf51c4c1a0cc07e18
Eric J. Humphrey <ejhumphrey@nyu.edu>
2013-10-01 15:32:01 -0400

    Top level utilities the dnn library will need to see

---
+++ dnn/utils.py
@@ -0,0 +1,60 @@
+"""
+"""
+
+import json
+import os
+import time
+
+TIME_FMT = "%Y%m%d_%H%M%S"
+
+def convert(obj):
+    """Convert unicode to strings.
+
+    Known issue: Uses dictionary comprehension, and is incompatible with 2.6.
+    """
+    if isinstance(obj, dict):
+        return {convert(key): convert(value) for key, value in obj.iteritems()}
+    elif isinstance(obj, list):
+        return [convert(element) for element in obj]
+    elif isinstance(obj, unicode):
+        return obj.encode('utf-8')
+    else:
+        return obj
+
+def timestamp():
+    """Returns a string representation of the time, like:
+    YYYYMMDD_HHMMSSmMMM
+    """
+    return time.strftime(TIME_FMT) + "m%03d" % int((time.time() % 1) * 1000)
+
+
+def json_save(obj, filename):
+    """Serialize data to disk.
+
+    Parameters
+    ----------
+    obj : iterable
+        Data structure to serialize.
+    filename : string
+        Path to write data.
+    """
+    base_directory = os.path.split(filename)[0]
+    if not os.path.exists(base_directory):
+        os.makedirs(base_directory)
+
+    # Save json-encoded architecture.
+    file_handle = open(filename, "w")
+    json.dump(obj, file_handle, indent=2)
+    file_handle.close()
+
+def json_load(filename):
+    """Serialize data to disk.
+
+    Parameters
+    ----------
+    obj : iterable
+        Data structure to serialize.
+    filename : string
+        Path to write data.
+    """
+    return convert(json.load(open(filename)))
