93554208601f83e71950292ef8260dba3ab9337d
Eric J. Humphrey <ejhumphrey@nyu.edu>
2013-10-02 21:07:36 -0400

    Modified script to consume a file of lab_files / file bases and a directory pointing to flat directory of numpy arrays

--- scripts/load_chords_into_hewey.py
+++ scripts/load_chords_into_hewey.py
@@ -2,9 +2,9 @@
 
 Sample Call:
 $ ipython ejhumphrey/scripts/load_chords_into_hewey.py \
-/Volumes/Audio/Chord_Recognition/labeled_tracks_20130926_train0.txt \
-/Volumes/Audio/Chord_Recognition/cqt_params.txt \
-/Volumes/speedy/chordrec_20130930_train0.dsf
+/Volumes/Audio/Chord_Recognition/labeled_tracks_20130930_train0.txt \
+/Volumes/Audio/Chord_Recognition/cqts/20131002 \
+/Volumes/speedy/chordrec_lcn_train0_20131002.dsf
 """
 
 import argparse
@@ -13,44 +13,41 @@ import numpy as np
 import os
 import time
 
-from ejhumphrey.datasets import chordutils
+from ejhumphrey.datasets import chordutils, utils
 from marl.hewey.core import DataSequence
 from marl.hewey.file import DataSequenceFile
 from marl.hewey.keyutils import uniform_keygen
 
 
-def collect_track_tuples(filelist):
-    """Compile a list of (audio_file, cqt_file, label_file) tuples.
+def pair_cqt_to_lab_file(lab_file, cqt_directory):
+    """Use a lab_file to recover a corresponding cqt_file.
 
-    It is guaranteed that all files exist on disk.
+    Parameters
+    ----------
+    lab_file : str
+        File (or file base) to key on.
+    cqt_directory : str
+        Path to search.
 
     Returns
     -------
-    results : list
-        List of three item tuples; audio, cqt, and label.
+    cqt_file : str
+        Absolute file path to a numpy array; guaranteed to exist.
     """
-    results = []
-    for line in open(filelist):
-        audio_file = line.strip('\n')
-        assert os.path.exists(audio_file), \
-            "Audio file does not exist: %s" % audio_file
-        cqt_file = os.path.splitext(audio_file)[0] + "-cqt.npy"
-        assert os.path.exists(cqt_file), \
-            "CQT file does not exist: %s" % cqt_file
-        label_file = os.path.splitext(audio_file)[0] + ".lab"
-        assert os.path.exists(label_file), \
-            "Label file does not exist: %s" % label_file
-        results.append((audio_file, cqt_file, label_file))
-
-    return results
+    cqt_file = utils.expand_filebase(
+        utils.filebase(lab_file), cqt_directory, "npy")
+    assert os.path.exists(cqt_file), "Could not find file: %s" % cqt_file
+    return cqt_file
 
 
-def create_datasequence_file(tracklist, filename, cqt_params):
+def create_datasequence_file(split_file, cqt_directory, filename, cqt_params):
     """
     Parameters
     ----------
-    tracklist : string
-        Path to a text file of absolute file paths.
+    split_file : string
+        Text file containing a list of lab files.
+    cqt_directory : string
+        Path to search for CQT numpy files.
     filename : string
         Output name for the DataSequenceFile
     cqt_params : dict
@@ -58,14 +55,15 @@ def create_datasequence_file(tracklist, filename, cqt_params):
     """
     file_handle = DataSequenceFile(filename)
     keygen = uniform_keygen(2)
-    for i, tuples in enumerate(collect_track_tuples(tracklist)):
-        audio_file, cqt_file, label_file = tuples
-        print "%03d: Importing %s" % (i, audio_file)
+    for i, line in enumerate(open(split_file)):
+        lab_file = line.strip("\n")
+        cqt_file = pair_cqt_to_lab_file(lab_file, cqt_directory)
+        print "%03d: Importing %s" % (i, cqt_file)
         cqt_array = np.load(cqt_file)
         labels = chordutils.align_lab_file_to_array(
-            cqt_array, label_file, cqt_params.get("framerate"))
+            cqt_array, lab_file, cqt_params.get("framerate"))
         metadata = {"timestamp": time.asctime(),
-                    "filesource": audio_file}
+                    "filesource": cqt_file}
         dseq = DataSequence(value=cqt_array, label=labels, metadata=metadata)
         key = keygen.next()
         file_handle.write(key, dseq)
@@ -74,24 +72,34 @@ def create_datasequence_file(tracklist, filename, cqt_params):
 
 
 def main(args):
-    cqt_params = json.load(open(args.cqt_param_file))
-    create_datasequence_file(args.track_filelist, args.output_file, cqt_params)
+    cqt_params_file = os.path.join(args.cqt_directory, "cqt_params.txt")
+    assert os.path.exists(cqt_params_file), \
+        "CQT param file does not exist: %s" % cqt_params_file
+    cqt_params = json.load(open(cqt_params_file))
+    create_datasequence_file(
+        args.split_file, args.cqt_directory, args.output_file, cqt_params)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-        description="Push CQT files through a trained deepnet.")
+        description="Import CQT arrays into a DataSequenceFile for training.")
 
-    parser.add_argument("track_filelist",
-                        metavar="track_filelist", type=str,
-                        help="File list of CQT arrays to import.")
+    parser.add_argument("split_file",
+                        metavar="split_file", type=str,
+                        help="List of lab files to import; probably a training "
+                        "or validation split.")
 
-    parser.add_argument("cqt_param_file",
-                        metavar="cqt_param_file", type=str,
-                        help="Parameters used to compute the CQT.")
+    parser.add_argument("cqt_directory",
+                        metavar="track_split", type=str,
+                        help="Base path to search for CQT arrays. Must also "
+                        "contain a cqt_params.txt file.")
+
+#    parser.add_argument("cqt_param_file",
+#                        metavar="cqt_param_file", type=str,
+#                        help="Parameters used to compute the CQT.")
 
     parser.add_argument("output_file",
                         metavar="output_file", type=str,
-                        help="Filepath to write the output Hewey filesystem.")
+                        help="Filepath to write the DataSequenceFile.")
 
     main(parser.parse_args())
