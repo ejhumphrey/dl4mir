73dc3b2895a58ec43af20f9a6d8675355dd38e5c
Eric J. Humphrey <ejhumphrey@nyu.edu>
2013-10-09 23:31:37 -0400

    Added L2 Constraint functionality for Affine and Conv3D weights

--- dnn/core/updates.py
+++ dnn/core/updates.py
@@ -6,6 +6,7 @@ import theano
 import theano.tensor as T
 from . import FLOATX
 from collections import OrderedDict
+from ejhumphrey.dnn.core import functions
 
 
 def sgd(scalar_loss, params):
@@ -103,3 +104,57 @@ class SGD(object):
 
     def empty_inputs(self, fill_value=0.0):
         return dict([(x.name, fill_value) for x in self.inputs])
+
+
+def L2NormTensor4(tensor4, **ignored_args):
+    """Each tensor3 along the first dimension is scaled to unit norm.
+    """
+    scalar = T.pow(T.pow(tensor4.flatten(2), 2.0).sum(axis=1), 0.5)
+    return tensor4 / scalar.dimshuffle(0, 'x', 'x', 'x')
+
+def L2NormMatrix(weight_matrix, **ignored_args):
+    """
+    It is assumed that the first dimension of the weight_matrix is the input,
+    and second is the output. The contributions of the input are supposed to be
+    bounded, so the axes are transposed compared to the normal case, i.e.
+    L2-norm the columns.
+    """
+    scalar = T.pow(T.pow(weight_matrix, 2.0).sum(axis=0), 0.5)
+    return weight_matrix / scalar.dimshuffle('x', 0)
+
+
+RegisteredConstraints = {'l2norm-matrix': L2NormMatrix,
+                         'l2norm-tensor4': L2NormTensor4}
+
+class Constraints(object):
+
+    def __init__(self):
+        self._updates = OrderedDict()
+        self._fx = None
+
+    def register(self, param, args):
+        """
+        Parameters
+        ----------
+        param : theano.shared type
+            Symbolic parameter to constrain.
+        args : dict
+            Arguments for the given constraint. Must contain at least a "name".
+        """
+        assert not param in self._updates, \
+            "%s already has an associated constraint, consider merging." % param
+
+        self._updates[param] = RegisteredConstraints.get(
+            args.get("name"))(param, **args)
+
+    def compile(self):
+        self._fx = theano.function(inputs=[],
+                                   outputs=None,
+                                   allow_input_downcast=True,
+                                   updates=self._updates)
+
+    def apply(self):
+        """"""
+        if self._fx is None:
+            self.compile()
+        self._fx()
