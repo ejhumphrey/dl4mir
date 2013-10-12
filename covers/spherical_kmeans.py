ddc2e09c5f46bb6829e2dc63f6a5496a88883756
Eric J. Humphrey <ejhumphrey@nyu.edu>
2013-10-12 15:29:51 -0400

    Spherical kMeans is stable and running well

--- covers/spherical_kmeans.py
+++ covers/spherical_kmeans.py
@@ -6,55 +6,12 @@ ipython
 """
 
 import argparse
-from multiprocessing import Pool
 import numpy as np
 import theano
 import theano.tensor as T
 import time
-from scipy.spatial.distance import cdist, euclidean
-
-
-# Global dict
-class Globals():
-    def __init__(self):
-        self.k_centroids = 0
-        self.feature_dim = None
-        self.num_samples = None
-        self.centroids = np.zeros([])
-        self.assignment_buffer = []
-        self.data = None
-
-    def clear(self):
-        self.assignment_buffer = []
-
-    def load_data(self, input_file):
-        self.data = np.load(input_file).astype('float32')
-        self.data -= self.data.mean(axis=0)
-        self.data /= self.data.std(axis=0)
-        self.num_samples = self.data.shape[0]
-        self.feature_dim = self.data.shape[1]
-
-    def init_centroids(self):
-        self.centroids = np.random.normal(
-            size=[self.k_centroids, self.feature_dim])
-        self.norm_centroids()
-        self.initial_centroids = self.centroids.copy()
-
-    def norm_centroids(self):
-        u = cdist(self.centroids, np.zeros([1, self.feature_dim]), 'euclidean')
-        self.centroids /= u
-
-glowballz = Globals()
-
-
-def compile_theano_dot():
-    x_in = T.matrix(name="x")
-    D_mat = T.matrix(name="D")
-    return theano.function(inputs=[x_in, D_mat],
-                           outputs=T.dot(x_in, D_mat.T),
-                           allow_input_downcast=True)
-
-tdot = compile_theano_dot()
+from scipy.spatial.distance import cdist
+import cPickle
 
 
 def data_stepper(data, step_size=1000):
@@ -62,49 +19,111 @@ def data_stepper(data, step_size=1000):
     for m in xrange(M):
         yield data[m * step_size : (m + 1) * step_size]
 
-def update_centroids():
-    step_size = 1000
-    new_centroids = glowballz.centroids.copy()
-    print "[%s] Starting cluster assignment." % (time.asctime())
-    for x_m in data_stepper(glowballz.data, step_size):
-        S = tdot(x_m, glowballz.centroids)
-        centroid_index = S.argmax(axis=1)
-        S_ij = S[np.arange(len(S)), centroid_index]
-        new_centroids[centroid_index, :] += S_ij[:, np.newaxis] * x_m
+class SphereicalKMeans(object):
+    def __init__(self, k_centroids, standardize=True, whiten=False, step_size=1000):
+        self.k_centroids = k_centroids
+        self.standardize = standardize
+        self.whiten = whiten
+        self._step_size = step_size
 
-    glowballz.centroids = new_centroids
-    glowballz.norm_centroids()
-    print "[%s] Finished update." % time.asctime()
+        self.feature_dim = None
+        self.num_samples = None
+        self.centroids = np.zeros([])
+        self.data = None
+        self._compile_theano_dot()
+
+    def _init_centroids(self):
+        self.centroids = np.random.normal(
+            size=[self.k_centroids, self.feature_dim])
+        self._norm_centroids()
+        self.initial_centroids = self.centroids.copy()
+        self.percent_change = np.inf
+
+    def _norm_centroids(self):
+        u = cdist(self.centroids, np.zeros([1, self.feature_dim]), 'euclidean')
+        self.centroids /= u
+
+    def _compile_theano_dot(self):
+        x_in = T.matrix(name="x")
+        D_mat = T.matrix(name="D")
+        self.dot = theano.function(inputs=[x_in, D_mat],
+                               outputs=T.dot(x_in, D_mat.T),
+                               allow_input_downcast=True)
+
+    def update_centroids(self, data, verbose=True):
+        new_centroids = self.centroids.copy()
+        if verbose:
+            print "[%s] Starting cluster assignment." % (time.asctime())
+
+        assignment = []
+        for x_m in data_stepper(data, self._step_size):
+            S = self.dot(x_m, self.centroids)
+            centroid_index = S.argmax(axis=1)
+            assignment.extend(centroid_index.tolist())
+            S_ij = S[np.arange(len(S)), centroid_index]
+            new_centroids[centroid_index, :] += S_ij[:, np.newaxis] * x_m
+
+        assignment = np.array(assignment)
+        self.percent_change = 100 * np.mean(self.previous_assignment != assignment)
+        self.previous_assignment = assignment
+        self.centroids = new_centroids
+        self._norm_centroids()
+        if verbose:
+            print "[%s] Finished update, %0.3f%% changed." % \
+                (time.asctime(), self.percent_change)
+
+    def fit(self, data, stopping_diff=0.01, max_epochs=1000, verbose=False):
+        if self.standardize:
+            self.mu = data.mean(axis=0)[np.newaxis, :]
+            self.sigma = data.std(axis=0)[np.newaxis, :]
+            data -= self.mu
+            data /= self.sigma
+
+        if self.whiten:
+            raise NotImplementedError("ZCA whitening doesn't exist yet.")
+
+        self.num_samples = data.shape[0]
+        self.feature_dim = data.shape[1]
+
+        self._init_centroids()
+        self.previous_assignment = np.zeros(self.num_samples) - 1
+
+        epoch = 0
+        while epoch < max_epochs and self.percent_change > stopping_diff:
+            self.update_centroids(data, verbose=verbose)
 
 
 
 def main(args):
     """Main routine for staging parallelization."""
 
-    glowballz.k_centroids = args.k_centroids
-    glowballz.load_data(args.input_file)
-    glowballz.init_centroids()
+    kmeans = SphereicalKMeans(args.k_centroids)
+    data = np.load(args.input_file).astype('float32')
+    try:
+        kmeans.fit(data, max_epochs=args.max_epochs, verbose=True)
+    except KeyboardInterrupt:
+        print "Stopping early."
 
-    for n in xrange(args.max_iterations):
-        update_centroids()
-
-    print [euclidean(u, v) for u, v in zip(glowballz.centroids, glowballz.initial_centroids)]
+    fh = open(args.output_file, 'w')
+    cPickle.dump({'mu':kmeans.mu,
+                  'sig':kmeans.sigma,
+                  'centroids':kmeans.centroids}, fh)
+    fh.close()
 
 
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="")
+    parser = argparse.ArgumentParser(description="")
     parser.add_argument("input_file",
-                        metavar="input_file", type=str,
-                        help="Numpy file of input data, shaped (samples, features).")
+                       metavar="input_file", type=str,
+                      help="Numpy file of input data, shaped (samples, features).")
     parser.add_argument("k_centroids",
                         metavar="k_centroids", type=int,
                         help="Number of centroids to compute.")
-    parser.add_argument("max_iterations",
-                        metavar="max_iterations", type=int,
-                        help="Maximum number of iterations before quitting.")
+    parser.add_argument("max_epochs",
+                        metavar="max_epochs", type=int,
+                        help="Maximum number of epochs before stopping.")
     parser.add_argument("output_file",
                         metavar="output_file", type=str,
                         help="File to save the result.")
