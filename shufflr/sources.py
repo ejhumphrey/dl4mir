50a9366e190618018bea6c3301ce75379b6a3162
Eric J. Humphrey <ejhumphrey@nyu.edu>
2014-01-22 19:56:18 -0500

    Added some Batch classes, but these may need to be moved to their own module

--- shufflr/sources.py
+++ shufflr/sources.py
@@ -310,22 +310,71 @@ class Cache(dict):
         self._clear_tables()
 
 
-class Batch(OrderedDict):
+class LabelBatch(OrderedDict):
     """
     """
-    def __init__(self, source, batch_size):
+    def __init__(self, source, batch_size, label_key, value_shape):
         OrderedDict.__init__(self)
         self.source = source
-        self.batch_size = batch_size
+        self._batch_size = batch_size
+        self._label_key = label_key
+        self._value_shape = value_shape
 
-    def next(self):
+    def refresh(self):
         self.clear()
-        self.load(num_items=self.batch_size)
-        return [self[k] for k in self]
+        self.load(num_items=self._batch_size)
+
+    def values(self):
+        return np.array([np.reshape(self[k].value, self._value_shape)
+                         for k in self])
+
+    def labels(self):
+        return np.array([self[k].labels[self._label_key] for k in self])
 
     def load(self, num_items=1):
         """Load the next 'num_items'."""
-        while num_items > 0:
-            k, v = self.source.next()
-            self[k] = v
-            num_items -= 1
+        # while num_items > 0:
+            # k, v = self.source.next()
+            # self[k] = v
+            # num_items -= 1
+        for n in xrange(num_items):
+            self.update(dict([self.source.next()]))
+
+
+class PairedBatch(LabelBatch):
+    """
+    """
+    def values_A(self):
+        return self.values()[self._idx_A]
+
+    def values_B(self):
+        return self.values()[self._idx_B]
+
+    def equals(self):
+        return np.equal(self.labels()[self._idx_A],
+                        self.labels()[self._idx_B]).astype(float)
+
+    def refresh(self):
+        LabelBatch.refresh(self)
+        self._pair()
+
+    def _pair(self):
+        self._idx_A, self._idx_B = [], []
+        N = len(self)
+        labels = self.labels()
+        for n in range(self._batch_size/2):
+            y_idx = np.random.randint(N)
+            possible_idx = list(np.arange(N)[labels == labels[y_idx]])
+            possible_idx.remove(y_idx)
+            self._idx_A.append(y_idx)
+            self._idx_B.append(choice(possible_idx))
+
+        for n in range(self._batch_size/2):
+            y_idx = np.random.randint(N)
+            possible_idx = list(np.arange(N)[labels != labels[y_idx]])
+            self._idx_A.append(y_idx)
+            self._idx_B.append(choice(possible_idx))
+
+        M = min([len(self._idx_A), len(self._idx_B)])
+        self._idx_A = np.asarray(self._idx_A)[:M]
+        self._idx_B = np.asarray(self._idx_B)[:M]
