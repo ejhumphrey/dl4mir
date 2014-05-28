397987c14cef9061a9faca01f21d4ff832a3b423
Eric J. Humphrey <ejhumphrey@nyu.edu>
2014-05-28 09:47:31 +0200

    Encapsulated training driver, but it's still doing all its own iteration.

--- dl4mir/chords/train_classifiers.sh
+++ dl4mir/chords/train_classifiers.sh
@@ -22,15 +22,15 @@ PARAM_TEXTLIST="paramlist.txt"
 
 if [ -z "$1" ]; then
     echo "Usage:"
-    echo "train_classifiers.sh {driver|all} {[0-5]|all}"
-    echo $'\tdriver - Name of the training driver, or all.'
-    echo $'\tfold - Number of the training fold, or all.'
+    echo "train.sh {driver|all} {[0-4]|*all}"
+    echo $'\tdriver - Name of the training driver.'
+    echo $'\tfold# - Number of the training fold, default=all.'
     exit 0
 fi
 
 if [ "$1" == "all" ]
 then
-    echo "Setting all drivers"
+    echo "Setting all known drivers..."
     DRIVER="classifier-L05-V157 "\
 # "classifier-L10-V157 "\
 # "classifier-L20-V157 "\
@@ -38,55 +38,63 @@ then
 "classifier-L80-V157"
     exit 0
 else
-    DRIVER="classifier-L$1-V157"
+    DRIVER="$1"
+fi
+
+if [ "$2" == "all"] || [ -z "$2" ];
+then
+    echo "Setting all folds"
+    FOLD_IDXS=$(seq 0 4)
+else
+    FOLD_IDXS=$2
 fi
 
 # Train networks
-for (( idx=0; idx< 5; idx++ ))
+for drv in ${DRIVER}
 do
-    for drv in DRIVER
+    for idx in ${FOLD_IDXS}
     do
-        python ${SRC}/chords/drivers/${DRIVER}.py \
+        python ${SRC}/chords/drivers/${drv}.py \
 ${OPTFILES}/${idx}/train.hdf5 \
-${MODELS}/${DRIVER}/${idx} \
+${MODELS}/${drv}/${idx} \
 ${TRIAL_NAME} \
 ${VALIDATOR_NAME}.json \
 ${TRANSFORM_NAME}.json
     done
 done
 
-echo "Collecting parameters."
-python ${SRC}/common/collect_files.py \
-${MODELS}/${DRIVER}/${idx}/${TRIAL_NAME} \
-"*.npz" \
-${MODELS}/${DRIVER}/${idx}/${TRIAL_NAME}/${PARAM_TEXTLIST}
-
 # Model Selection
-for (( idx=0; idx< 5; idx++ ))
+for drv in ${DRIVER}
 do
-    for drv in DRIVER
+    for idx in ${FOLD_IDXS}
     do
+        echo "Collecting parameters."
+        python ${SRC}/common/collect_files.py \
+${MODELS}/${drv}/${idx}/${TRIAL_NAME} \
+"*.npz" \
+${MODELS}/${drv}/${idx}/${TRIAL_NAME}/${PARAM_TEXTLIST}
+
         python ${SRC}/chords/select_params.py \
 ${OPTFILES}/${idx}/valid.hdf5 \
-${MODELS}/${DRIVER}/${idx}/${TRIAL_NAME}/${VALIDATOR_NAME}.json \
-${MODELS}/${DRIVER}/${idx}/${TRIAL_NAME}/${PARAM_TEXTLIST} \
-${MODELS}/${DRIVER}/${idx}/${TRIAL_NAME}/${TRANSFORM_NAME}.npz
+${MODELS}/${drv}/${idx}/${TRIAL_NAME}/${VALIDATOR_NAME}.json \
+${MODELS}/${drv}/${idx}/${TRIAL_NAME}/${PARAM_TEXTLIST} \
+${MODELS}/${drv}/${idx}/${TRIAL_NAME}/${TRANSFORM_NAME}.npz
     done
 done
 
 # Transform data
-for (( idx=0; idx< 5; idx++ ))
+for drv in ${DRIVER}
 do
-    for drv in DRIVER
+    for idx in ${FOLD_IDXS}
     do
         for split in valid train test
         do
             echo "Transforming ${OPTFILES}/${idx}/${split}.hdf5"
             python ${SRC}/chords/transform_data.py \
 ${OPTFILES}/${idx}/${split}.hdf5 \
-${MODELS}/${DRIVER}/${idx}/${TRIAL_NAME}/${TRANSFORM_NAME}.json \
-${MODELS}/${DRIVER}/${idx}/${TRIAL_NAME}/${TRANSFORM_NAME}.npz \
-${OUTPUTS}/${DRIVER}/${idx}/${TRIAL_NAME}/${split}.hdf5
+${MODELS}/${drv}/${idx}/${TRIAL_NAME}/${TRANSFORM_NAME}.json \
+${MODELS}/${drv}/${idx}/${TRIAL_NAME}/${TRANSFORM_NAME}.npz \
+${OUTPUTS}/${drv}/${idx}/${TRIAL_NAME}/${split}.hdf5
         done
     done
 done
