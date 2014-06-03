95debbfa994ff8b4b14136d11365f60350e4b8bc
Eric J. Humphrey <ejhumphrey@nyu.edu>
2014-06-03 16:07:09 +0200

    Removed mir_eval dependencies in guitar.__init__.py

--- dl4mir/guitar/__init__.py
+++ dl4mir/guitar/__init__.py
@@ -1,4 +1,3 @@
-import mir_eval.chord as _chord
 
 ROOTS = ['C', 'C#', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'Ab', 'A', 'Bb', 'B']
 
@@ -8,29 +7,6 @@ QUALITIES = {
     157: ['maj', 'min', 'maj7', 'min7', '7', 'maj6', 'min6', 'dim', 'aug',
           'sus4', 'sus2', 'aug', 'dim7', 'hdim7'],
 }
-
-_QINDEX = dict([(v, dict([(tuple(_chord.QUALITIES[q]), i)
-                          for i, q in enumerate(QUALITIES[v])]))
-                for v in QUALITIES])
-
-
-def get_quality_index(semitomes, vocab):
-    return _QINDEX[vocab].get(tuple(semitomes), None)
-
-
-def parts_to_index(root, semitones, vocab):
-    if root < 0:
-        return vocab - 1
-    q_idx = get_quality_index(semitones, vocab)
-    if q_idx is None:
-        return q_idx
-    return root + q_idx*12
-
-
-def index_to_chord_label(index, vocab):
-    if index == vocab - 1:
-        return "N"
-    return "%s:%s" % (ROOTS[index % 12], QUALITIES[157][int(index) / 12])
 
 DRIVER_ARGS = dict(
     max_iter=1000,
