81d4f35861694dd548c4ee9292aec9a85213779e
Eric J. Humphrey <ejhumphrey@nyu.edu>
2014-06-05 13:28:56 +0200

    Added beat-sync code and script to export outputs to mats

---
+++ dl4mir/chords/beat_sync_entities.py
@@ -0,0 +1,67 @@
+"""Utility to dump an optimus file to a flat collection of Matlab files."""
+import argparse
+import marl.fileutils as futils
+import numpy as np
+from os import path
+import optimus
+import json
+import time
+
+
+def beat_sync(entity, new_times, chord_codes=None):
+    new_times = list(new_times)
+    data = entity.values
+    time_points = data.pop('time_points')
+    if not time_points[0] in new_times:
+        new_times.insert(0, time_points[0])
+    if not time_points[-1] in new_times:
+        new_times.append(time_points[-1])
+    new_times.sort()
+
+    idxs = np.array([(t > time_points).astype(int).argmin() - 1
+                     for t in new_times[:-1]])
+    for key in data:
+        shape = data[key].shape
+        if len(time_points) - 1 in shape:
+            data[key] = data[key][idxs, ...]
+    if chord_codes:
+        data['chord_codes'] = chord_codes
+
+    return optimus.Entity(time_points=new_times, **data)
+
+
+def main(args):
+    dset = optimus.File(args.input_file)
+    futils.create_directory(path.split(args.output_file)[0])
+    dout = optimus.File(args.output_file)
+    beat_times = json.load(open(args.beat_times))
+    total_count = len(dset)
+    chord_codes = dict()
+    if args.chord_codes:
+        chord_codes.update(json.load(open(args.chord_codes)))
+
+    for idx, key in enumerate(dset.keys()):
+        dout.add(key, beat_sync(dset.get(key), beat_times[key],
+                                chord_codes.get(key, None)))
+        print "[%s] %12d / %12d: %s" % (time.asctime(), idx, total_count, key)
+
+    dout.close()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Beat-synchronize a dataset of entities.")
+    parser.add_argument("input_file",
+                        metavar="input_file", type=str,
+                        help="Path to the input optimus file.")
+    parser.add_argument("beat_times",
+                        metavar="beat_times", type=str,
+                        help="JSON file of new times; keys must match.")
+    parser.add_argument("output_file",
+                        metavar="output_file", type=str,
+                        help="Path to the output optimus file.")
+    parser.add_argument("--chord_codes",
+                        metavar="--chord_codes", type=str, default='',
+                        help="JSON file of TMC's lab segment data; "
+                        " keys must match.")
+    main(parser.parse_args())
---
+++ dl4mir/chords/matlab_export.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+#
+# Train a set of end-to-end classifiers and sweep over the checkpointed
+#    parameters to identify the early stopping point.
+
+BASEDIR=/media/attic/dl4mir/chord_estimation
+SRC=~/Dropbox/NYU/marldev/src/ejhumphrey/dl4mir
+
+# Directory of optimus data files, divided by index and split, like
+METADATA=${BASEDIR}/metadata
+OUTPUTS=${BASEDIR}/outputs
+MATS=${BASEDIR}/mats
+
+if [ -z "$1" ]; then
+    echo "Usage:"
+    echo "train.sh {driver|all} {[0-4]|*all} {name|all}"
+    echo $'\tdriver - Name of the training driver.'
+    echo $'\tfold# - Number of the training fold, default=all.'
+    exit 0
+fi
+
+if [ "$1" == "all" ]
+then
+    echo "Setting all known drivers..."
+    DRIVER="chroma-L05 "\
+"tonnetz-L05 "\
+"classifier-L05-V157 "\
+"classifier-L10-V157 "\
+"classifier-L20-V157 "\
+"classifier-L40-V157 "\
+"classifier-L80-V157"
+else
+    DRIVER="$1"
+fi
+
+if [ "$2" == "all" ] || [ -z "$2" ];
+then
+    echo "Setting all folds"
+    FOLD_IDXS=$(seq 0 4)
+else
+    FOLD_IDXS=$2
+fi
+
+TRIAL_NAME=$3
+
+for drv in ${DRIVER}
+do
+    for idx in ${FOLD_IDXS}
+    do
+        for split in valid test train
+        do
+            echo "Synchronizing ${OUTPUTS}/${drv}/${idx}/${TRIAL_NAME}/${split}.hdf5"
+            python ${SRC}/chords/beat_sync_entities.py \
+${OUTPUTS}/${drv}/${idx}/${TRIAL_NAME}/${split}.hdf5 \
+${METADATA}/beat_times.json \
+${OUTPUTS}/${drv}/${idx}/${TRIAL_NAME}/${split}-beatsync.hdf5 \
+--chord_codes=${METADATA}/chord_codes.json
+
+            echo "Exporting ${OUTPUTS}/${drv}/${idx}/${TRIAL_NAME}/${split}-beatsync.hdf5"
+            python ${SRC}/common/export_optimus_file_to_mats.py \
+${OUTPUTS}/${drv}/${idx}/${TRIAL_NAME}/${split}-beatsync.hdf5 \
+${MATS}/${drv}/${idx}/${TRIAL_NAME}/${split}-beatsync
+        done
+    done
+done
+
+
