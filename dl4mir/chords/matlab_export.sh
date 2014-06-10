3a6b3a6480a7e06572bc28f29fdbf40d1138166d
Eric J. Humphrey <ejhumphrey@nyu.edu>
2014-06-10 15:33:54 +0200

    Updates to matlab exporting, no longer iterates over drivers

--- dl4mir/chords/matlab_export.sh
+++ dl4mir/chords/matlab_export.sh
@@ -9,57 +9,41 @@ SRC=~/Dropbox/NYU/marldev/src/ejhumphrey/dl4mir
 # Directory of optimus data files, divided by index and split, like
 METADATA=${BASEDIR}/metadata
 OUTPUTS=${BASEDIR}/outputs
-MATS=${BASEDIR}/mats
+FEATURES=${BASEDIR}/features
 
 if [ -z "$1" ]; then
     echo "Usage:"
-    echo "train.sh {driver|all} {[0-4]|*all} {name|all}"
+    echo "train.sh driver trial {[0-4]|*all}"
     echo $'\tdriver - Name of the training driver.'
     echo $'\tfold# - Number of the training fold, default=all.'
     exit 0
 fi
 
-if [ "$1" == "all" ]
-then
-    echo "Setting all known drivers..."
-    DRIVER="chroma-L05 "\
-"tonnetz-L05 "\
-"classifier-L05-V157 "\
-"classifier-L10-V157 "\
-"classifier-L20-V157 "\
-"classifier-L40-V157 "\
-"classifier-L80-V157"
-else
-    DRIVER="$1"
-fi
+DRIVER=$1
+TRIAL=$2
 
-if [ "$2" == "all" ] || [ -z "$2" ];
+if [ "$3" == "all" ] || [ -z "$3" ];
 then
     echo "Setting all folds"
     FOLD_IDXS=$(seq 0 4)
 else
-    FOLD_IDXS=$2
+    FOLD_IDXS=$3
 fi
 
-TRIAL_NAME=$3
-
-for drv in ${DRIVER}
+for idx in ${FOLD_IDXS}
 do
-    for idx in ${FOLD_IDXS}
+    for split in valid test train
     do
-        for split in valid test train
-        do
-            echo "Synchronizing ${OUTPUTS}/${drv}/${idx}/${TRIAL_NAME}/${split}.hdf5"
-            python ${SRC}/chords/beat_sync_entities.py \
-${OUTPUTS}/${drv}/${idx}/${TRIAL_NAME}/${split}.hdf5 \
+        echo "Synchronizing ${OUTPUTS}/${DRIVER}/${idx}/${TRIAL}/${split}.hdf5"
+        python ${SRC}/chords/beat_sync_entities.py \
+${OUTPUTS}/${DRIVER}/${idx}/${TRIAL}/${split}.hdf5 \
 ${METADATA}/beat_times.json \
-${OUTPUTS}/${drv}/${idx}/${TRIAL_NAME}/${split}-beatsync.hdf5
+${OUTPUTS}/${DRIVER}/${idx}/${TRIAL}/${split}-beatsync.hdf5
 
-            echo "Exporting ${OUTPUTS}/${drv}/${idx}/${TRIAL_NAME}/${split}-beatsync.hdf5"
-            python ${SRC}/chords/export_chroma_to_mats.py \
-${OUTPUTS}/${drv}/${idx}/${TRIAL_NAME}/${split}-beatsync.hdf5 \
+        echo "Exporting ${OUTPUTS}/${DRIVER}/${idx}/${TRIAL}/${split}-beatsync.hdf5"
+        python ${SRC}/chords/export_chroma_to_mats.py \
+${OUTPUTS}/${DRIVER}/${idx}/${TRIAL}/${split}-beatsync.hdf5 \
 ${METADATA}/chord_codes.json \
-${MATS}/${drv}/${idx}/${TRIAL_NAME}
-        done
+${FEATURES}/${DRIVER}/${idx}/${TRIAL}
     done
 done
