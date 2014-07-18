91ed07a510c3fbe4f0650bb993a6dc83491aed82
Eric J. Humphrey <ejhumphrey@nyu.edu>
2014-07-18 12:46:37 -0400

    Added (and verified) pitch_shift to pipefxs

---
+++ chords/pipefxs.py
@@ -0,0 +1,39 @@
+import numpy as np
+from dl4mir.chords import labels
+
+import biggie
+from marl.utils.matrix import circshift
+from marl.chords.utils import transpose_chord_index
+
+
+def pitch_shift(max_pitch_shift=12, bins_per_pitch=3):
+    """Apply a random circular shift to the CQT, and rotate the root."""
+    def fx(entity):
+        # assert "cqt" in entity
+        # assert "chord_label" in entity
+
+        values = entity.values()
+        cqt, chord_label = values.pop('cqt'), values.pop('chord_label')
+
+        # Determine the amount of pitch-shift.
+        pitch_shift = np.random.randint(low=-max_pitch_shift,
+                                        high=max_pitch_shift)
+
+        # Change the chord label if it has a harmonic root.
+        chord_label = str(entity.chord_label.value)
+        if not chord_label in [labels.NO_CHORD, labels.SKIP_CHORD]:
+
+            root, quality, exts, bass = labels.split(chord_label)
+            root = (labels.pitch_class_to_semitone(root) + pitch_shift) % 12
+            new_root = labels.semitone_to_pitch_class(root)
+            new_label = labels.join(new_root, quality, exts, bass)
+            print "Input %12s // Shift: %3s // Output %12s" % \
+                (chord_label, pitch_shift, new_label)
+            chord_label = new_label
+
+        # Always rotate the CQT.
+
+        bin_shift = pitch_shift*bins_per_pitch
+        cqt = circshift(cqt[0], 0, bin_shift)[np.newaxis, ...]
+        return biggie.Entity(cqt=cqt, chord_label=chord_label, **values)
+    return fx
