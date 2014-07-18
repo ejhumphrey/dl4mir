b76091365a7a03a6c8e9478e29040969abe41b49
Eric J. Humphrey <ejhumphrey@nyu.edu>
2014-07-18 15:30:48 -0400

    Added label mapping functions

--- chords/pipefxs.py
+++ chords/pipefxs.py
@@ -27,13 +27,40 @@ def pitch_shift(max_pitch_shift=12, bins_per_pitch=3):
             root = (labels.pitch_class_to_semitone(root) + pitch_shift) % 12
             new_root = labels.semitone_to_pitch_class(root)
             new_label = labels.join(new_root, quality, exts, bass)
-            print "Input %12s // Shift: %3s // Output %12s" % \
-                (chord_label, pitch_shift, new_label)
+            # print "Input %12s // Shift: %3s // Output %12s" % \
+            #     (chord_label, pitch_shift, new_label)
             chord_label = new_label
 
         # Always rotate the CQT.
-
         bin_shift = pitch_shift*bins_per_pitch
         cqt = circshift(cqt[0], 0, bin_shift)[np.newaxis, ...]
         return biggie.Entity(cqt=cqt, chord_label=chord_label, **values)
+    return fx
+
+
+def map_to_chord_index(vocab_dim):
+    """
+    vocabulary: int
+    """
+    def fx(entity):
+        values = entity.values()
+        cqt, chord_label = values.pop('cqt'), str(values.pop('chord_label'))
+        chord_idx = labels.chord_label_to_class_index(chord_label, vocab_dim)
+        if chord_idx is None:
+            return None
+        return biggie.Entity(cqt=cqt, chord_idx=chord_idx)
+    return fx
+
+
+def map_to_chord_quality_index(vocab_dim):
+    """
+    vocabulary: int
+    """
+    def fx(entity):
+        values = entity.values()
+        cqt, chord_label = values.pop('cqt'), str(values.pop('chord_label'))
+        qual_idx = labels.chord_label_to_quality_index(chord_label, vocab_dim)
+        if qual_idx is None:
+            return None
+        return biggie.Entity(cqt=cqt, quality_idx=qual_idx)
     return fx
