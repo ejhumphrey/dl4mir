abac5b3487ad15b2d4a0fa57b219f4385685c92c
Eric J. Humphrey <ejhumphrey@nyu.edu>
2014-07-23 17:11:10 -0400

    Changed pipe functions to generators

--- chords/pipefxs.py
+++ chords/pipefxs.py
@@ -6,61 +6,86 @@ from marl.utils.matrix import circshift
 from marl.chords.utils import transpose_chord_index
 
 
-def pitch_shift(max_pitch_shift=12, bins_per_pitch=3):
-    """Apply a random circular shift to the CQT, and rotate the root."""
-    def fx(entity):
-        # assert "cqt" in entity
-        # assert "chord_label" in entity
+def _pitch_shift(entity, pitch_shift, bins_per_pitch):
+    values = entity.values()
+    cqt, chord_label = values.pop('cqt'), str(values.pop('chord_label'))
 
-        values = entity.values()
-        cqt, chord_label = values.pop('cqt'), values.pop('chord_label')
+    # Change the chord label if it has a harmonic root.
+    if not chord_label in [labels.NO_CHORD, labels.SKIP_CHORD]:
+        root, quality, exts, bass = labels.split(chord_label)
+        root = (labels.pitch_class_to_semitone(root) + pitch_shift) % 12
+        new_root = labels.semitone_to_pitch_class(root)
+        new_label = labels.join(new_root, quality, exts, bass)
+        # print "Input %12s // Shift: %3s // Output %12s" % \
+        #     (chord_label, pitch_shift, new_label)
+        chord_label = new_label
+
+    # Always rotate the CQT.
+    bin_shift = pitch_shift*bins_per_pitch
+    cqt = circshift(cqt[0], 0, bin_shift)[np.newaxis, ...]
+    return biggie.Entity(cqt=cqt, chord_label=chord_label, **values)
+
+
+def pitch_shift(stream, max_pitch_shift=12, bins_per_pitch=3):
+    """Apply a random circular shift to the CQT, and rotate the root."""
+    for entity in stream:
+        if entity is None:
+            yield entity
+            continue
 
         # Determine the amount of pitch-shift.
-        pitch_shift = np.random.randint(low=-max_pitch_shift,
-                                        high=max_pitch_shift)
-
-        # Change the chord label if it has a harmonic root.
-        chord_label = str(entity.chord_label.value)
-        if not chord_label in [labels.NO_CHORD, labels.SKIP_CHORD]:
-
-            root, quality, exts, bass = labels.split(chord_label)
-            root = (labels.pitch_class_to_semitone(root) + pitch_shift) % 12
-            new_root = labels.semitone_to_pitch_class(root)
-            new_label = labels.join(new_root, quality, exts, bass)
-            # print "Input %12s // Shift: %3s // Output %12s" % \
-            #     (chord_label, pitch_shift, new_label)
-            chord_label = new_label
-
-        # Always rotate the CQT.
-        bin_shift = pitch_shift*bins_per_pitch
-        cqt = circshift(cqt[0], 0, bin_shift)[np.newaxis, ...]
-        return biggie.Entity(cqt=cqt, chord_label=chord_label, **values)
-    return fx
+        shift = np.random.randint(low=-max_pitch_shift,
+                                  high=max_pitch_shift)
+        yield _pitch_shift(entity, shift, bins_per_pitch)
 
 
-def map_to_chord_index(vocab_dim):
+def map_to_chord_index(stream, vocab_dim):
     """
-    vocabulary: int
+    vocab_dim: int
     """
-    def fx(entity):
+    for entity in stream:
+        if entity is None:
+            yield entity
+            continue
         values = entity.values()
         cqt, chord_label = values.pop('cqt'), str(values.pop('chord_label'))
         chord_idx = labels.chord_label_to_class_index(chord_label, vocab_dim)
-        if chord_idx is None:
-            return None
-        return biggie.Entity(cqt=cqt, chord_idx=chord_idx)
-    return fx
+        yield None if chord_idx is None else biggie.Entity(cqt=cqt,
+                                                           chord_idx=chord_idx)
 
 
-def map_to_chord_quality_index(vocab_dim):
+def map_to_chord_quality_index(stream, vocab_dim):
     """
-    vocabulary: int
+    vocab_dim: int
     """
-    def fx(entity):
+    for entity in stream:
+        if entity is None:
+            yield entity
+            continue
         values = entity.values()
         cqt, chord_label = values.pop('cqt'), str(values.pop('chord_label'))
         qual_idx = labels.chord_label_to_quality_index(chord_label, vocab_dim)
-        if qual_idx is None:
-            return None
-        return biggie.Entity(cqt=cqt, quality_idx=qual_idx)
-    return fx
+        yield None if qual_idx is None else biggie.Entity(cqt=cqt,
+                                                          quality_idx=qual_idx)
+
+def unpack_contrastive_pairs(stream, vocab_dim, rotate_prob=0.75):
+    """
+    vocab_dim: int
+    """
+    for pair in stream:
+        if pair is None:
+            yield pair
+            continue
+        pos_entity, neg_entity = pair
+        pos_chord_label = str(pos_entity.chord_label.value)
+        neg_chord_label = str(neg_entity.chord_label.value)
+        pos_chord_idx = labels.chord_label_to_class_index(pos_chord_label, vocab_dim)
+        neg_chord_idx = labels.chord_label_to_class_index(neg_chord_label, vocab_dim)
+        if np.random.binomial(1, rotate_prob):
+            shift = (pos_chord_idx - neg_chord_idx) % 12
+            neg_entity = _pitch_shift(neg_entity, shift, 3)
+        # print pos_entity.chord_label.value, neg_entity.chord_label.value
+        yield biggie.Entity(cqt=pos_entity.cqt.value,
+                            chord_idx=pos_chord_idx, is_chord=1)
+        yield biggie.Entity(cqt=neg_entity.cqt.value,
+                            chord_idx=pos_chord_idx, is_chord=0)
