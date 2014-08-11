7d96bac2f7edcb68b6017886566dc16b8889fdc1
Eric J. Humphrey <ejhumphrey@nyu.edu>
2014-08-11 08:11:34 -0400

    Updated labfile aggregation to current mir_eval

--- chords/aggregate_labfile_estimations.py
+++ chords/aggregate_labfile_estimations.py
@@ -8,6 +8,7 @@ import mir_eval
 import os
 import marl.fileutils as futil
 from dl4mir.chords import labels
+import time
 
 
 def align_estimation_to_reference(est_file, ref_file, num_classes=157):
@@ -26,8 +27,8 @@ def align_estimation_to_reference(est_file, ref_file, num_classes=157):
         Chord labels and dense count vectors.
     """
     predictions = dict()
-    est_intervals, est_labels = mir_eval.io.load_intervals(est_file)
-    ref_intervals, ref_labels = mir_eval.io.load_intervals(ref_file)
+    est_intervals, est_labels = mir_eval.io.load_labeled_intervals(est_file)
+    ref_intervals, ref_labels = mir_eval.io.load_labeled_intervals(ref_file)
     t_min = ref_intervals.min()
     t_max = ref_intervals.max()
     ref_intervals, ref_labels = mir_eval.util.filter_labeled_intervals(
@@ -59,10 +60,15 @@ def align_estimation_to_reference(est_file, ref_file, num_classes=157):
 def main(args):
     results_files = glob.glob(os.path.join(args.results_dir, "*.txt"))
     predictions = dict()
-    for result_file in results_files:
-        lab_file = os.path.join(args.labs_dir, os.path.split(result_file)[-1])
-        key = futil.filebase(lab_file)
-        predictions[key] = align_estimation_to_reference(result_file, lab_file)
+    for idx, result_file in enumerate(results_files):
+        key = futil.filebase(result_file.replace(".txt", ''))
+        lab_file = os.path.join(args.labs_dir, "%s.lab" % key)
+        try:
+            predictions[key] = align_estimation_to_reference(result_file, lab_file)
+        except IndexError:
+            print "Index error: %s" % result_file
+        print "[%s] %12d / %12d: %s" % (time.asctime(), idx,
+                                        len(results_files), key)
 
     futil.create_directory(os.path.split(args.output_file)[0])
     with open(args.output_file, 'w') as fp:
