731d2210d50548df7ebb792dadfacda3b2788c7c
Eric J. Humphrey <ejhumphrey@nyu.edu>
2014-08-11 08:18:47 -0400

    Updated chroma exporting to actually work

--- chords/export_chroma_to_mats.py
+++ chords/export_chroma_to_mats.py
@@ -12,30 +12,32 @@ import time
 FILE_EXT = "mp3.mat"
 
 
-def entity_to_mdict(entity):
-    beats_in_time = entity.time_points.value[:-1]
+def entity_to_mdict(entity, labseg):
     chroma = entity.chroma.value.T
+    time_points = entity.time_points.value
+    labseg = np.array(labseg, dtype=np.uint32)
+    # This is a gross hack. Where is this +1 index issue coming from?
+    L = min([chroma.shape[1], len(time_points), len(labseg)])
     if chroma.shape[0] != 12:
         raise ValueError(
             "First dimension of chroma must be 12, shape=%s" % chroma.shape)
     chroma_obj = np.empty([1, 1], dtype=object)
-    chroma_obj[0, 0] = chroma
-
+    chroma_obj[0, 0] = chroma[:, :L]
     return dict(
-        beats_in_time=beats_in_time.reshape(len(beats_in_time), 1),
+        time_points=time_points[:L].reshape(L, 1),
         chroma=chroma_obj,
-        endT=np.array([[entity.time_points.value[-1]]]))
+        labseg=labseg[:L],
+        endT=float(entity.endT.value))
 
 
 def main(args):
-    dset = biggie.File(args.input_file)
+    dset = biggie.Stash(args.input_file)
     labseg = json.load(open(args.labseg))
     out_dir = futils.create_directory(args.output_directory)
     total_count = len(dset)
     for idx, key in enumerate(dset.keys()):
         out_file = path.join(out_dir, "%s.%s" % (key, FILE_EXT))
-        mdict = dict(labseg=np.array(labseg[key], dtype=np.uint32))
-        mdict.update(entity_to_mdict(dset.get(key)))
+        mdict = entity_to_mdict(dset.get(key), labseg[key])
         MLAB.savemat(out_file, mdict=mdict)
         print "[%s] %12d / %12d: %s" % (time.asctime(), idx, total_count, key)
 
