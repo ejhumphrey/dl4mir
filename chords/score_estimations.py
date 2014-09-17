a36ab2e6308b04bf70d29c2b8762a732ac0c2e60
Eric J. Humphrey <ejhumphrey@nyu.edu>
2014-09-17 10:00:45 -0400

    Updated all docstrings to scoring and made the functions far more sane / callable from a validation process

--- chords/score_estimations.py
+++ chords/score_estimations.py
@@ -1,43 +1,14 @@
-"""Apply a graph convolutionally to datapoints in an optimus file."""
+"""Compute final evaluation metrics and confusions for chord estimations."""
 
 import argparse
 import json
 import numpy as np
 import os
-import mir_eval
 import marl.fileutils as futil
 import sklearn.metrics as metrics
-import scipy.signal as signal
 import warnings
 
 from dl4mir.chords import labels
-
-
-def evaluate_posterior(entity, medfilt=0):
-    posterior, chord_labels = entity.posterior, entity.chord_labels
-    vocab_dim = posterior.shape[1]
-    chord_idxs = labels.chord_label_to_class_index(chord_labels, vocab_dim)
-    if medfilt > 0:
-        posterior = signal.medfilt(posterior, [medfilt, 1])
-        posterior /= posterior.sum(axis=1)[:, np.newaxis]
-    pred_idxs = posterior.argmax(axis=1)
-    L = min([len(pred_idxs), len(chord_idxs)])
-    return np.equal(pred_idxs[:L], chord_idxs[:L]).sum(), L
-
-
-def load_chord_index_pair(reference_file, estimated_file, vocab_dim=157):
-    ref_intervals, ref_labels = labels.load_labeled_intervals(reference_file)
-    est_intervals, est_labels = labels.load_labeled_intervals(estimated_file)
-
-    (intervals,
-     ref_labels,
-     est_labels) = mir_eval.chord.align_labeled_intervals(
-        ref_intervals, ref_labels, est_intervals, est_labels, 'fit-to-ref')
-
-    ref_idxs = labels.chord_label_to_class_index(ref_labels, vocab_dim)
-    est_idxs = labels.chord_label_to_class_index(est_labels, vocab_dim)
-
-    return np.abs(np.diff(intervals, axis=1)).flatten(), ref_idxs, est_idxs
 
 
 def collapse_estimations(estimations):
@@ -66,11 +37,15 @@ def collapse_estimations(estimations):
 
 def confusion_matrix(results, num_classes,
                      label_to_index=labels.chord_label_to_class_index):
-    """
+    """Deprecated: Don't use.
 
-    Confusion matrix is actual x estimations.
+    Note: Confusion matrix is actual x estimations.
 
+    Parameters
+    ----------
+    ??
     """
+    raise NotImplementedError("Get outta here!")
     classifications = np.zeros([num_classes, num_classes])
     for label, counts in results.items():
         idx = label_to_index(label, num_classes)
@@ -81,6 +56,18 @@ def confusion_matrix(results, num_classes,
 
 
 def quality_confusion_matrix(results):
+    """Populate a quality-rotated confusion matrix.
+
+    Parameters
+    ----------
+    results : dict
+        A flattened dictionary of chord labels, pointing to count vectors.
+
+    Returns
+    -------
+    confusions : np.ndarrays
+        The quality-rotated confusion matrix.
+    """
     num_classes = 157
     qual_conf = np.zeros([num_classes, num_classes])
     chord_idxs = labels.chord_label_to_class_index(results.keys(), num_classes)
@@ -94,12 +81,26 @@ def quality_confusion_matrix(results):
     return qual_conf
 
 
-def compute_confusions(quality_confusions, top_k=5):
-    if quality_confusions.shape[0] == 157:
-        quality_confusions = quality_confusions[::12, :]
+def confusions_to_str(confusions, top_k=5):
+    """Render the confusion matrix to human-readable text.
+
+    Parameters
+    ----------
+    confusions : np.ndarray, shape=(num_classes, num_classes)
+        Quality-rotated confusions.
+    top_k : int
+        Number of top confusions to print.
+
+    Returns
+    -------
+    text : str
+        Pretty confusions.
+    """
+    if confusions.shape[0] == 157:
+        confusions = confusions[::12, :]
 
     outputs = []
-    for idx, row in enumerate(quality_confusions):
+    for idx, row in enumerate(confusions):
         row /= float(row.sum())
         line = "%7s (%7.4f) ||" % (labels.index_to_chord_label(idx*12, 157),
                                    row[idx*12]*100)
@@ -115,55 +116,98 @@ def compute_confusions(quality_confusions, top_k=5):
             k += 1
         # print line
         outputs.append(line)
-    return "\n".join(outputs)
+    return "\n".join(outputs) + "\n"
 
 
 def compute_scores(estimations):
+    """Compute scores over a dataset.
+
+    Parameters
+    ----------
+    estimations : dict
+        A set of chord tallies, where chord labels point to a vector of counts.
+
+    Returns
+    -------
+    stats : dict
+        Weighted and averaged (classwise) statistics over the estimations.
+    confusions : np.ndarray
+        Confusion matrix over the estimations.
+    """
     results = collapse_estimations(estimations)
-    # confusions = confusion_matrix(results, 157)
-    quality_confusions = quality_confusion_matrix(results)
-    # chord_true, chord_est = confusions_to_comparisons(confusions)
-    quality_true, quality_est = confusions_to_comparisons(quality_confusions)
-    output = []
+    confusions = quality_confusion_matrix(results)
+    quality_true, quality_est = confusions_to_comparisons(confusions)
+    stats = dict()
     with warnings.catch_warnings():
         labels = range(157)
         warnings.simplefilter("ignore")
 
-        qual_precision_weighted = metrics.precision_score(
+        stats['precision_weighted'] = metrics.precision_score(
             quality_true, quality_est, labels=labels, average='weighted')
-        qual_precision_ave = np.mean(metrics.precision_score(
+        stats['precision_averaged'] = np.mean(metrics.precision_score(
             quality_true, quality_est, labels=labels, average=None)[::12])
 
-        qual_recall_weighted = metrics.recall_score(
+        stats['recall_weighted'] = metrics.recall_score(
             quality_true, quality_est, labels=labels, average='weighted')
-        qual_recall_ave = np.mean(metrics.recall_score(
+        stats['recall_averaged'] = np.mean(metrics.recall_score(
             quality_true, quality_est, labels=labels, average=None)[::12])
 
-        qual_f1_weighted = metrics.f1_score(
+        stats['f1_weighted'] = metrics.f1_score(
             quality_true, quality_est, labels=labels, average='weighted')
-        qual_f1_ave = np.mean(metrics.f1_score(
+        stats['f1_averaged'] = np.mean(metrics.f1_score(
             quality_true, quality_est, labels=labels, average=None)[::12])
 
+    return stats, confusions
+
+
+def stats_to_string(stats):
+    """Render the stats dictionary to human readable text.
+
+    Parameters
+    ----------
+    stats : dict
+        Has the following keys:
+            [precision_weighted, recall_weighted, f1_weighted,
+             precision_averaged, recall_averaged, f1_averaged,]
+
+    Returns
+    -------
+    text : str
+        Pretty stats.
+    """
     stat_line = "  Precision: %0.4f\t Recall: %0.4f\tf1: %0.4f"
-    res1 = "Weighted: " + stat_line % (100*qual_precision_weighted,
-                                       100*qual_recall_weighted,
-                                       100*qual_f1_weighted)
+    res1 = "Weighted: " + stat_line % (100*stats['precision_weighted'],
+                                       100*stats['recall_weighted'],
+                                       100*stats['f1_weighted'])
 
-    res2 = "Averaged: " + stat_line % (100*qual_precision_ave,
-                                       100*qual_recall_ave,
-                                       100*qual_f1_ave)
+    res2 = "Averaged: " + stat_line % (100*stats['precision_ave'],
+                                       100*stats['recall_ave'],
+                                       100*stats['f1_ave'])
     res3 = "-"*72
     outputs = [res3, res1, res2, res3]
-    outputs.append(compute_confusions(quality_confusions, 5))
+    outputs.append()
     return "\n".join(outputs) + '\n'
 
 
-def confusions_to_comparisons(conf_mat):
+def confusions_to_comparisons(confusions):
+    """Convert a confusion matrix to sklearn-style aligned class predictions.
+
+    Parameters
+    ----------
+    confusions : np.ndarray, shape=(num_classes, num_classes)
+        Overall confusion matrix; rows are actual classes, columns are
+        predicted classes.
+
+    Returns
+    -------
+    y_true, y_pred : np.ndarrays, shape=(N,)
+        The actual and predicted class labels of the observations.
+    """
     y_true, y_pred = [], []
-    conf_mat = np.round(conf_mat).astype(int)
-    for i in range(conf_mat.shape[0]):
-        for j in range(conf_mat.shape[1]):
-            count = conf_mat[i, j]
+    confusions = np.round(confusions).astype(int)
+    for i in range(confusions.shape[0]):
+        for j in range(confusions.shape[1]):
+            count = confusions[i, j]
             y_true.append(i + np.zeros(count, dtype=int))
             y_pred.append(j + np.zeros(count, dtype=int))
     return np.concatenate(y_true), np.concatenate(y_pred)
@@ -173,12 +217,13 @@ def main(args):
     if not os.path.exists(args.estimation_file):
         print "File does not exist: %s" % args.estimation_file
         return
-    stats = compute_scores(json.load(open(args.estimation_file)))
-
+    stats, confusions = compute_scores(json.load(open(args.estimation_file)))
+    res_str = stats_to_string(stats) + confusions_to_str(confusions)
     futil.create_directory(os.path.split(args.stats_file)[0])
     with open(args.stats_file, 'w') as fp:
-        fp.write(stats)
-    print "\n\n%s\n%s"  % (args.estimation_file, stats)
+        fp.write(res_str)
+    print "\n%s\n%s" % (args.estimation_file, res_str)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="")
