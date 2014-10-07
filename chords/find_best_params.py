8e679a345068a02599e3a7b37ce52a67bc3f4740
Eric J. Humphrey <ejhumphrey@nyu.edu>
2014-10-07 19:10:33 -0400

    Added validation stats updating to find_best_params

--- chords/find_best_params.py
+++ chords/find_best_params.py
@@ -40,15 +40,19 @@ def sweep_stash(stash, transform, p_vals):
 
 
 def sweep_param_files(param_files, stash, transform, p_vals,
-                      lexicon, log_file):
+                      lexicon, log_file, overwrite=False):
     param_stats = dict([(f, dict()) for f in param_files])
+    if os.path.exists(log_file):
+        param_stats.update(json.load(open(log_file)))
+
     for count, f in enumerate(param_files):
         try:
             transform.load_param_values(f)
             stash_estimations = sweep_stash(stash, transform, p_vals)
             for p in p_vals:
-                param_stats[f][p] = SE.compute_scores(stash_estimations[p],
-                                                      lexicon)[0]
+                if not param_stats[f].get(p) or overwrite:
+                    param_stats[f][p] = SE.compute_scores(stash_estimations[p],
+                                                          lexicon)[0]
                 stat_str = SE.stats_to_string(param_stats[f][p])
                 print "[%s] %s (%0.3f) \n%s" % (time.asctime(), f, p, stat_str)
             with open(log_file, 'w') as fp:
