b72076a778aadb00bae450795833ecaa681db6a6
Eric J. Humphrey <ejhumphrey@nyu.edu>
2014-11-22 12:11:19 -0500

    Untested mods to output labeled intervals

--- chords/output_labels.py
+++ chords/output_labels.py
@@ -1,78 +1,44 @@
-"""Apply a graph convolutionally to datapoints in a biggie Stash."""
+"""Viterbi decode a stash of posteriors and output labeled intervals."""
 
 import argparse
-import numpy as np
 import json
-from marl.chords.utils import viterbi_alg
-from scipy import signal
 import biggie
 import os
 import marl.fileutils as futils
 import time
 
-from dl4mir.chords.labels import index_to_chord_label
+from dl4mir.chords.lexicon import Strict
 from dl4mir.common.util import run_length_encode
+from dl4mir.common.util import viterbi
 
 
-def compress_samples_to_intervals(labels, framerate):
-    framerate = float(framerate)
+def compress_samples_to_intervals(labels, time_points):
     intervals, new_labels = [], []
-    last_end = -1.0/framerate
-    for l, duration in run_length_encode(labels):
-        intervals += [[last_end, last_end + duration / framerate]]
-        last_end = intervals[-1][-1]
-        new_labels += [l]
-    intervals[0][0] = 0.0
+    idx = 0
+    for label, count in run_length_encode(labels):
+        start, end = time_points[idx], time_points[idx + count]
+        idx += count
+        intervals += [(start, end)]
+        new_labels += [label]
     return intervals, new_labels
 
 
-def predict_posterior(posterior, viterbi_penalty=0, medfilt=0):
-    """Transform a cqt-based entity with a given network.
-
-    Parameters
-    ----------
-    posterior: np.ndarray
-        Posteriorgram of chord classes.
-    viterbi_penalty: scalar, in (0, inf)
-        Self-transition penalty; higher values produce more "stable" paths.
-
-    Returns
-    -------
-    labels: list
-        List of chord label names (str).
-    """
-
-    if medfilt > 0:
-        posterior = signal.medfilt(posterior, [medfilt, 1])
-        posterior /= posterior.sum(axis=1)[:, np.newaxis]
-
-    if viterbi_penalty > 0:
-        indexes = viterbi_alg(posterior, rho=viterbi_penalty)
-    else:
-        indexes = posterior.argmax(axis=1)
-
-    vocab = posterior.shape[1]
-    return [index_to_chord_label(idx, vocab) for idx in indexes]
-
-
-def posterior_to_labeled_intervals(posterior, framerate=20.0,
-                                   viterbi_penalty=0.0, medfilt=0):
-    labels = predict_posterior(posterior, viterbi_penalty, medfilt)
-    return compress_samples_to_intervals(labels, framerate)
+def posterior_to_labeled_intervals(entity, penalty, vocab):
+    y_idx = viterbi(entity.posterior, penalty=penalty)
+    labels = vocab.index_to_label(y_idx)
+    return compress_samples_to_intervals(labels, entity.time_points)
 
 
 def main(args):
-    dset = biggie.Stash(args.posterior_file)
+    stash = biggie.Stash(args.posterior_file)
     output_dir = futils.create_directory(args.output_directory)
-
-    framerate = json.load(open(args.cqt_params))['framerate']
-    total_count = len(dset.keys())
-    for idx, key in enumerate(dset.keys()):
+    stats = json.load(open(args.validation_file))
+    penalty = float(stats['best_config']['penalty'])
+    vocab = Strict(157)
+    total_count = len(stash.keys())
+    for idx, key in enumerate(stash.keys()):
         intervals, labels = posterior_to_labeled_intervals(
-            dset.get(key).posterior,
-            framerate=framerate,
-            viterbi_penalty=args.viterbi_penalty,
-            medfilt=args.medfilt)
+            stash.get(key), penalty=penalty, vocab=vocab)
         print "[%s] %12d / %12d: %s" % (time.asctime(), idx, total_count, key)
         output_file = os.path.join(output_dir, "%s.json" % key)
         with open(output_file, 'w') as fp:
@@ -85,15 +51,9 @@ if __name__ == "__main__":
     parser.add_argument("posterior_file",
                         metavar="posterior_file", type=str,
                         help="Path to an biggie stash of chord posteriors.")
-    parser.add_argument("viterbi_penalty",
-                        metavar="viterbi_penalty", type=float,
-                        help="Penalty for the viterbi algorithm (skip if 0).")
-    parser.add_argument("medfilt",
-                        metavar="medfilt", type=int,
-                        help="Length of median filter; must be odd.")
-    parser.add_argument("cqt_params",
-                        metavar="cqt_params", type=str,
-                        help="JSON file containing parameters of the cqt.")
+    parser.add_argument("validation_file",
+                        metavar="validation_file", type=str,
+                        help="")
     # Outputs
     parser.add_argument("output_directory",
                         metavar="output_directory", type=str,
