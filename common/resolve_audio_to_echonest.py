c18cf79e71c7f64a2cb6c5ff59564c19c11da05b
Eric J. Humphrey <ejhumphrey@nyu.edu>
2014-11-24 13:30:20 -0500

    Added EchoNest track resolution script

---
+++ common/resolve_audio_to_echonest.py
@@ -0,0 +1,131 @@
+"""
+"""
+
+import argparse
+import json
+import marl.fileutils as futil
+import glob
+from pyechonest import track as T
+from os import path
+import time
+
+
+class Throttle(object):
+    def __init__(self, delay=5.0):
+        self.last_check = time.time()
+        self.delay = delay
+
+    def touch(self):
+        self.last_check = time.time()
+
+    def is_ready(self, delay=None):
+        if delay is None:
+            delay = self.delay
+        return time.time() - delay > self.last_check
+
+    def wait(self, delay=None):
+        if delay:
+            self.touch()
+        while not self.is_ready(delay):
+            pass
+        self.touch()
+
+
+def extract_info(track):
+    data = dict()
+    for k, v in track.__dict__.items():
+        if k.startswith("_"):
+            continue
+        data[k] = v
+
+    return data
+
+
+def fetch_data(filepaths, result=None, overwrite=False, checkpoint_file=''):
+    """
+    Parameters
+    ----------
+    filepaths : list
+        Collection of audio files on disk to query against the EchoNest API.
+    result : dict, or None
+        Dictionary to add info; will create if None.
+    overwrite : bool, default=False
+        If False, will skip any keys contained in `result`.
+    checkpoint_file : str, or None
+        Path to write results as they are accumulated; ignored if empty.
+
+    Returns
+    -------
+    result : dict
+        Map of filebases to metadata.
+    """
+    throttle = Throttle()
+    throttle.touch()
+    if result is None:
+        result = dict()
+
+    filepaths = set(filepaths)
+    while filepaths:
+        fpath = filepaths.pop()
+        key = futil.filebase(fpath)
+        # If we've already got data and we're not overwriting, move on.
+        if key in result and not overwrite:
+            print "[%s] %4d: '%s'" % (time.asctime(), len(filepaths), key)
+            continue
+        try:
+            # Otherwise, let's make some requests.
+            print "[%s] %4d: '%s'" % (time.asctime(), len(filepaths), key)
+            track = T.track_from_filename(fpath)
+            if track:
+                result[key] = extract_info(track)
+            if checkpoint_file:
+                with open(checkpoint_file, 'w') as fp:
+                    json.dump(result, fp, indent=2)
+            throttle.wait()
+        except T.util.EchoNestAPIError as err:
+            if err.http_status == 429:
+                print "You got rate limited braah ... hang on."
+                throttle.wait(10)
+                filepaths.add(fpath)
+            elif err.http_status >= 500:
+                print "Server error; moving on, dropping key: %s" % key
+            else:
+                print "Caught unexpected error? %s" % err
+            throttle.wait(10)
+    return result
+
+
+def main(args):
+    filepaths = glob.glob(
+        path.join(args.audio_directory, "*.%s" % args.ext.strip(".")))
+    if path.exists(args.output_file):
+        result = json.load(open(args.output_file))
+        print "File exists: Found %d results" % len(result)
+    else:
+        futil.create_directory(path.split(args.output_file)[0])
+        result = dict()
+
+    result = fetch_data(
+        filepaths, result=result, overwrite=False,
+        checkpoint_file=args.output_file)
+
+    with open(args.output_file, 'w') as fp:
+        json.dump(result, fp, indent=2)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Convert a directory of labeled interval files to JAMS.")
+    parser.add_argument("audio_directory",
+                        metavar="audio_directory", type=str,
+                        help="Path to a directory of audio files.")
+    parser.add_argument("output_file",
+                        metavar="output_file", type=str,
+                        help="Path for the output metadata.")
+    parser.add_argument("--ext",
+                        metavar="--ext", type=str, default='mp3',
+                        help="File extension to glob against.")
+    parser.add_argument("--overwrite",
+                        metavar="--overwrite", type=bool, default=False,
+                        help="Overwrite any existing results.")
+    main(parser.parse_args())
