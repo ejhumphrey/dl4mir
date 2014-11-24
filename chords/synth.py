a2e937648566642ff6d3590b57d8d2b8075b3353
Eric J. Humphrey <ejhumphrey@nyu.edu>
2014-11-24 10:36:25 -0500

    Heavy mods to chord audio synthesis

--- chords/synth.py
+++ chords/synth.py
@@ -8,13 +8,27 @@ import random
 import time
 import marl.fileutils as futil
 
-from multiprocessing import Pool
-
+import dl4mir.common.util as util
+# from multiprocessing import Pool
 
 NUM_CPUS = 12
 
+
 def normalize(x_n):
     return x_n / np.abs(x_n).max()
+
+
+def hwr(x):
+    return x * (x >= 0)
+
+
+def flange(x_n, fs, gain=0.5, depth=0.001, delay=0.005, freq=5.0):
+    N = len(x_n)
+    n_delay = fs * delay
+    n_depth = fs * depth
+    offset = np.round(n_depth * np.sin(2*np.pi*np.arange(N)*freq/fs) + n_delay)
+    dline = (np.arange(N) - offset).astype(int)
+    return x_n + gain * x_n[hwr(dline)] * (dline > 0)
 
 
 def load_many(filenames, samplerate, channels=1, normalize_signal=False):
@@ -138,8 +152,17 @@ def sequence_signals(signals, intervals, samplerate=44100,
     else:
         assert duration >= intervals.max()
     num_samples = int(samplerate * duration)
-    output_buffer = np.zeros([num_samples, signals[0].shape[1]])
+    num_channels = None
+    for x_n in signals:
+        if hasattr(x_n, 'shape'):
+            num_channels = x_n.shape[1]
+            break
+    if num_channels is None:
+        raise ValueError("All signals are empty? wtf?")
+    output_buffer = np.zeros([num_samples, num_channels])
     for start, dur, x_n in zip(intervals[:, 0], durations, signals):
+        if x_n is None:
+            continue
         num_samples = int(dur * samplerate)
         x_n = envelope(x_n[:num_samples], samplerate, **env_args)
         idx = int(start * samplerate)
@@ -179,6 +202,29 @@ def random_symbolic_chord_sequence(chord_set, num_chords, repeat_prob=0.5):
     return chord_labels, note_numbers
 
 
+def random_polyphonic_sequence(poly_probs, pitch_probs, num_collections):
+    """
+    Parameters
+    ----------
+
+
+    Returns
+    -------
+    chord_labels : list
+        Chord label names for each chord.
+    note_numbers : list
+        List of midi note numbers to sound in each chord.
+    """
+    note_numbers = []
+    for _ in range(num_collections):
+        num_notes = util.categorical_sample(poly_probs)
+        pitches = set()
+        while len(pitches) < num_notes:
+            pitches.add(util.categorical_sample(pitch_probs))
+        note_numbers.append(list(pitches))
+    return note_numbers
+
+
 def random_drum_signal(drum_set, duration=30, samplerate=44100, channels=1):
     """
     Parameters
@@ -216,8 +262,12 @@ def random_drum_signal(drum_set, duration=30, samplerate=44100, channels=1):
     return y_n, np.concatenate(beat_times)
 
 
-def random_chord_signal(intervals, instrument_set, notes, amplitudes=None,
-                        samplerate=44100, env_args=None):
+def random_vsl_files_for_notes(instrument_set, note_numbers):
+    return [random.choice(instrument_set[n]) for n in note_numbers]
+
+
+def random_note_signal(intervals, instrument_set, notes, amplitudes=None,
+                       samplerate=44100, env_args=None):
     """
     Parameters
     ----------
@@ -252,15 +302,15 @@ def random_chord_signal(intervals, instrument_set, notes, amplitudes=None,
         List of string chord labels.
     """
 
-    def random_vsl_files_for_notes(instrument_set, note_numbers):
-        return [random.choice(instrument_set[n]) for n in note_numbers]
-
     # signals = load_many(filenames, samplerate, normalize)
     # return combine(signals)
 
     signals = []
     amplitudes = np.ones(len(intervals)) if amplitudes is None else amplitudes
     for nts, amp in zip(notes, amplitudes):
+        if not nts:
+            signals.append(None)
+            continue
         instrument_files = random_vsl_files_for_notes(instrument_set, nts)
         note_signals = load_many(instrument_files, samplerate=samplerate,
                                  channels=1, normalize_signal=True)
@@ -292,7 +342,7 @@ def random_noise_signal(audio_files, intervals, amplitudes=None,
     files = [random.choice(audio_files) for _ in range(len(intervals))]
     signals = load_many(files, samplerate, channels=1, normalize_signal=True)
     amplitudes = np.ones(len(intervals)) if amplitudes is None else amplitudes
-    return sequence_signals([a*x for a,x in zip(amplitudes, signals)],
+    return sequence_signals([a*x for a, x in zip(amplitudes, signals)],
                             intervals, samplerate, env_args)
 
 
@@ -321,8 +371,9 @@ def start_times_to_intervals(start_times, end_duration=None):
     return np.asarray(intervals + [(start_times[-1], end_duration)])
 
 
-def make_one(drum_set, chord_set, instrument_set, noise_files, weights=None,
-             duration=30, samplerate=44100, min_beat_period=0.5):
+def make_one_chord(drum_set, chord_set, instrument_set, noise_files,
+                   weights=None, duration=30, samplerate=44100,
+                   min_beat_period=0.5):
     drum_signal, beat_times = random_drum_signal(
         drum_set, samplerate=samplerate, duration=duration)
     if np.abs(np.diff(beat_times)).mean() < min_beat_period:
@@ -332,12 +383,12 @@ def make_one(drum_set, chord_set, instrument_set, noise_files, weights=None,
     # -- Generate chords --
     chord_labels, notes = random_symbolic_chord_sequence(
         chord_set, len(intervals))
-    chord_signal = random_chord_signal(
+    chord_signal = random_note_signal(
         intervals, instrument_set, notes, samplerate=samplerate)
 
     # -- Generate noise --
     # Subsample intervals
-    idx = np.random.binomial(1,.5, size=len(intervals)).astype(bool)
+    idx = np.random.binomial(1, 0.5, size=len(intervals)).astype(bool)
     noise_signal = random_noise_signal(
         noise_files, intervals[idx], samplerate=samplerate)
 
@@ -347,50 +398,101 @@ def make_one(drum_set, chord_set, instrument_set, noise_files, weights=None,
     return y_out, chord_labels, intervals
 
 
-def render(n):
+def make_one_note_sequence(drum_set, instrument_set, noise_files, max_notes=8,
+                           mix_weights=None, duration=30, samplerate=44100,
+                           min_beat_period=0.5):
+    drum_signal, beat_times = random_drum_signal(
+        drum_set, samplerate=samplerate, duration=duration)
+    if np.abs(np.diff(beat_times)).mean() < min_beat_period:
+        beat_times = beat_times[::2]
+    intervals = start_times_to_intervals(beat_times)
+
+    # -- Generate note sequence --
+    pitch_probs = np.zeros(120)
+    for note_num in instrument_set:
+        pitch_probs[int(note_num)] = len(instrument_set[note_num])
+    pitch_probs /= float(pitch_probs.sum())
+
+    notes = random_polyphonic_sequence(
+        np.ones(max_notes + 1) / float(max_notes + 1),
+        pitch_probs, len(intervals))
+
+    note_signal = random_note_signal(
+        intervals, instrument_set, notes, samplerate=samplerate)
+
+    # -- Generate noise --
+    # Subsample intervals
+    idx = np.random.binomial(1, 0.5, size=len(intervals)).astype(bool)
+    noise_signal = random_noise_signal(
+        noise_files, intervals[idx], samplerate=samplerate)
+
+    mix_weights = np.ones(3) / 3.0 if mix_weights is None else mix_weights
+    signals = [drum_signal, note_signal, noise_signal]
+    y_out = combine([w * x for w, x in zip(mix_weights, signals)])
+    return y_out, notes, intervals
+
+
+def render_chords(n, drum_set, chord_set, instrument_set, noise_files,
+                  duration, audio_dir, lab_dir, samplerate=44100.0):
     weights = [np.random.uniform(0.25, 1.0),
                np.random.uniform(0.25, 0.5),
-               np.random.uniform(0.5, 0.75),]
+               np.random.uniform(0.5, 0.75)]
     try:
-        y_out, chord_labels, intervals = make_one(
-            SHARED_DATA['drum_set'], SHARED_DATA['chord_set'],
-            SHARED_DATA['instrument_set'], SHARED_DATA['noise_files'],
-            weights, SHARED_DATA['duration'], SHARED_DATA['samplerate'])
+        y_out, chord_labels, intervals = make_one_chord(
+            drum_set, chord_set,
+            instrument_set, noise_files,
+            weights, duration, samplerate)
     except ValueError:
         print "Died trying to render '%d', revisit..."
         return
     y_out *= np.random.uniform(0.25, 1.0) / np.abs(y_out).max()
-    apath = "%s/%04d.mp3" % (SHARED_DATA['audio_dir'], n)
-    marl.audio.write(apath, y_out, SHARED_DATA['samplerate'])
-    with open("%s/%04d.json" % (SHARED_DATA['lab_dir'], n), 'w') as fp:
+    apath = "%s/%04d.mp3" % (audio_dir, n)
+    marl.audio.write(apath, y_out, samplerate)
+    with open("%s/%04d.json" % (lab_dir, n), 'w') as fp:
         json.dump(
             dict(labels=chord_labels, intervals=intervals.tolist()),
             fp, indent=2)
     print "[%s] Finished %12d" % (time.asctime(), n)
 
 
-SHARED_DATA = dict()
+def render_polypitch(idx, drum_set, instrument_set, noise_files, duration,
+                     audio_dir, lab_dir, samplerate=44100.0):
+    mix_weights = [np.random.uniform(0.25, 1.0),
+                   np.random.uniform(0.25, 0.5),
+                   np.random.uniform(0.5, 0.75)]
+    y_out, note_numbers, intervals = make_one_note_sequence(
+        drum_set=drum_set, instrument_set=instrument_set, max_notes=8,
+        noise_files=noise_files, mix_weights=mix_weights, duration=duration,
+        samplerate=samplerate)
+
+    y_out *= np.random.uniform(0.25, 1.0) / np.abs(y_out).max()
+    apath = "%s/%04d.mp3" % (audio_dir, idx)
+    marl.audio.write(apath, y_out, samplerate)
+    with open("%s/%04d.json" % (lab_dir, idx), 'w') as fp:
+        json.dump(
+            dict(note_numbers=note_numbers, intervals=intervals.tolist()),
+            fp, indent=2)
+    print "[%s] Finished %12d" % (time.asctime(), idx)
+
+
 def main(args):
-    SHARED_DATA['audio_dir'] = futil.create_directory(
+    audio_dir = futil.create_directory(
         os.path.join(args.output_directory, "audio"))
-    SHARED_DATA['lab_dir'] = futil.create_directory(
+    lab_dir = futil.create_directory(
         os.path.join(args.output_directory, "labs"))
 
-    SHARED_DATA['drum_set'] = json.load(open(args.drum_set))
-    SHARED_DATA['instrument_set'] = dict(
+    drum_set = json.load(open(args.drum_set))
+    instrument_set = dict(
         [(int(k), v) for k, v in json.load(open(args.instrument_set)).items()])
 
-    SHARED_DATA['chord_set'] = json.load(open(args.chord_voicings))
-    SHARED_DATA['noise_files'] = glob.glob(os.path.join(args.noise_dir,
-                                                        "*.wav"))
+    noise_files = glob.glob(os.path.join(args.noise_dir, "*.wav"))
 
-    SHARED_DATA['duration'] = 60
-    SHARED_DATA['samplerate'] = 44100
+    duration = 180
+    samplerate = 44100.0
 
-    for n in range(args.num_files):
-        render(n)
-    pool.close()
-    pool.join()
+    for idx in range(args.num_files):
+        render_polypitch(idx, drum_set, instrument_set, noise_files, duration,
+                         audio_dir, lab_dir, samplerate)
 
 
 if __name__ == "__main__":
