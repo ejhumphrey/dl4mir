d213454d9ea47541b8dfd95048e83e0bf49e6d7f
Eric J. Humphrey <ejhumphrey@nyu.edu>
2014-11-26 16:51:25 -0500

    Properly parallelized viterbi decoding to jams output; need to rename posteriors_to_labeled_intervals

--- chords/posteriors_to_labeled_intervals.py
+++ chords/posteriors_to_labeled_intervals.py
@@ -6,45 +6,56 @@ import biggie
 import os
 import marl.fileutils as futils
 import time
+from multiprocessing import Pool
+
+import pyjams
 
 from dl4mir.chords.lexicon import Strict
-from dl4mir.common.util import run_length_encode
-from dl4mir.common.util import viterbi
+from dl4mir.chords import util as cutil
+from dl4mir.common import util
 
 
-def compress_samples_to_intervals(labels, time_points):
-    assert len(labels) == len(time_points)
-    intervals, new_labels = [], []
-    idx = 0
-    for label, count in run_length_encode(labels):
-        start = time_points[idx]
-        end = time_points[min([idx + count, len(labels) - 1])]
-        idx += count
-        intervals += [(start, end)]
-        new_labels += [label]
-    return intervals, new_labels
+NUM_CPUS = 12
 
 
-def posterior_to_labeled_intervals(entity, penalty, vocab):
-    y_idx = viterbi(entity.posterior, penalty=penalty)
-    labels = vocab.index_to_label(y_idx)
-    return compress_samples_to_intervals(labels, entity.time_points)
+def fx(args):
+    entity, penalty, vocab, idx, key = args
+    print "[%s] %12d / %s" % (time.asctime(), idx, key)
+    return cutil.posterior_to_labeled_intervals(entity, penalty, vocab)
+
+
+def arg_gen(stash, keys, penalty, vocab):
+    for idx, key in enumerate(keys):
+        entity = stash.get(key)
+        yield (biggie.Entity(**entity.values()), penalty, vocab, idx, key)
 
 
 def main(args):
     stash = biggie.Stash(args.posterior_file)
     output_dir = futils.create_directory(args.output_directory)
-    stats = json.load(open(args.validation_file))
+    stats = json.load(open(args.validation_stats))
     penalty = float(stats['best_config']['penalty'])
     vocab = Strict(157)
-    total_count = len(stash.keys())
-    for idx, key in enumerate(stash.keys()):
-        intervals, labels = posterior_to_labeled_intervals(
-            stash.get(key), penalty=penalty, vocab=vocab)
-        print "[%s] %12d / %12d: %s" % (time.asctime(), idx, total_count, key)
-        output_file = os.path.join(output_dir, "%s.json" % key)
-        with open(output_file, 'w') as fp:
-            json.dump(dict(intervals=intervals, labels=labels), fp, indent=2)
+
+    keys = stash.keys()
+    pool = Pool(processes=NUM_CPUS)
+    results = pool.map(fx, arg_gen(stash, keys, penalty, vocab))
+    pool.close()
+    pool.join()
+
+    for key, res in zip(keys, results):
+        intervals, labels = res
+        output_file = os.path.join(output_dir, "%s.jams" % key)
+        jam = pyjams.JAMS()
+        annot = jam.chord.create_annotation()
+        pyjams.util.fill_range_annotation_data(
+            intervals[:, 0], intervals[:, 1], labels, annot)
+        annot.annotation_metadata.data_source = 'machine estimation'
+        annot.annotation_metadata.annotator = dict(
+            from_file=args.posterior_file,
+            timestamp=time.asctime(),
+            **stats['best_config'])
+        pyjams.save(jam, output_file)
 
 
 if __name__ == "__main__":
@@ -54,11 +65,11 @@ if __name__ == "__main__":
     parser.add_argument("posterior_file",
                         metavar="posterior_file", type=str,
                         help="Path to an biggie stash of chord posteriors.")
-    parser.add_argument("validation_file",
-                        metavar="validation_file", type=str,
-                        help="")
+    parser.add_argument("validation_stats",
+                        metavar="validation_stats", type=str,
+                        help="Output of the validation parameter sweep.")
     # Outputs
     parser.add_argument("output_directory",
                         metavar="output_directory", type=str,
-                        help="Path for labeled intervals as JSON.")
+                        help="Path for output JAMS files.")
     main(parser.parse_args())
