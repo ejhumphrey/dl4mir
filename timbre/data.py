802b8bd8bab18e2301d49f924442104b2a81158d
Eric J. Humphrey <ejhumphrey@nyu.edu>
2014-12-20 15:34:39 -0500

    Framework for timbre-sim training; missing shell script

---
+++ timbre/data.py
@@ -0,0 +1,211 @@
+import itertools
+import numpy as np
+
+import biggie
+import pescador
+import mir_eval
+from dl4mir.chords import labels as L
+import dl4mir.chords.pipefxs as FX
+from dl4mir.common import util
+import dl4mir.chords.lexicon as lex
+
+
+def slice_cqt_entity(entity, length, idx=None):
+    """Return a windowed slice of a cqt Entity.
+
+    Parameters
+    ----------
+    entity : Entity, with at least {cqt, icode} fields
+        Observation to window.
+        Note that entity.cqt is shaped (num_channels, num_frames, num_bins).
+    length : int
+        Length of the sliced array.
+    idx : int, or None
+        Centered frame index for the slice, or random if not provided.
+
+    Returns
+    -------
+    sample: biggie.Entity with fields {data, label}
+        The windowed observation.
+    """
+    idx = np.random.randint(entity.cqt.shape[1]) if idx is None else idx
+    cqt = np.array([util.slice_tile(x, idx, length) for x in entity.cqt])
+    return biggie.Entity(data=cqt, label=entity.icode)
+
+
+def cqt_sampler(key, stash, win_length=20, index=None, max_samples=None,
+                sample_func=slice_cqt_entity):
+    """Generator for sampling windowed observations from an entity.
+
+    Parameters
+    ----------
+    key : str
+        Key for the entity of interest; must be consistent across both `stash`
+        and `index`, when the latter is provided.
+    stash : dict_like
+        Dict or biggie.Stash of entities.
+    win_length: int
+        Length of centered observation window for the CQT.
+    index: dict of index arrays, default=None
+        Indexing object for constrained sampling of the entity.
+        If provided, must have a np.ndarray of integers under `key`; otherwise,
+        this method will fail.
+    max_samples: int, or None
+        Maximum number of samples to return from this Generator; if None, runs
+        indefinitely.
+
+    Yields
+    ------
+    sample: biggie.Entity with fields {cqt, label}
+        The windowed observation.
+    """
+    entity = stash.get(key)
+    num_samples = entity.cqt.shape[1]
+    if index is None:
+        index = {key: np.arange(num_samples)}
+
+    valid_samples = index.get(key, [])
+    idx = np.inf
+    max_samples = np.inf if max_samples is None else max_samples
+    count = 0
+    while count < max_samples and len(valid_samples):
+        if idx >= len(valid_samples):
+            np.random.shuffle(valid_samples)
+            idx = 0
+        yield sample_func(entity, win_length, valid_samples[idx])
+        idx += 1
+        count += 1
+
+
+def cqt_buffer(entity, win_length=20, valid_samples=None):
+    """Generator for stepping windowed observations from an entity.
+
+    Parameters
+    ----------
+    entity : biggie.Entity
+        CQT entity to step through
+    win_length: int
+        Length of centered observation window for the CQT.
+
+    Yields
+    ------
+    sample: biggie.Entity with fields {cqt, chord_label}
+        The windowed observation.
+    """
+    num_samples = len(entity.chord_labels)
+    if valid_samples is None:
+        valid_samples = np.arange(num_samples)
+
+    idx = 0
+    count = 0
+    while count < len(valid_samples):
+        yield slice_cqt_entity(entity, win_length, valid_samples[idx])
+        idx += 1
+        count += 1
+
+
+def lazy_cqt_buffer(key, stash, win_length=20, index=None):
+    """Generator for stepping windowed chord observations from an entity; note
+    that the entity is not queried until the generator is called.
+
+    Parameters
+    ----------
+    key : str
+        Key for the entity of interest; must be consistent across both `stash`
+        and `index`, when the latter is provided.
+    stash : dict_like
+        Dict or biggie.Stash of chord entities.
+    win_length: int
+        Length of centered observation window for the CQT.
+
+    Yields
+    ------
+    sample: biggie.Entity with fields {cqt, chord_label}
+        The windowed chord observation.
+    """
+    entity = stash.get(key)
+    num_samples = len(entity.chord_labels)
+    if index is None:
+        index = {key: np.arange(num_samples)}
+
+    valid_samples = index.get(key, [])
+    for x in cqt_buffer(entity, win_length, valid_samples):
+        yield x
+
+
+def create_labeled_stream(stash, win_length, working_size=5000,
+                          sample_func=slice_cqt_entity):
+    """Return an unconstrained stream of samples with class labels.
+
+    Parameters
+    ----------
+    stash : biggie.Stash
+        A collection of chord entities.
+    win_length : int
+        Length of a given tile slice.
+    working_size : int
+        Number of open streams at a time.
+
+    Returns
+    -------
+    stream : generator
+        Data stream of windowed chord entities.
+    """
+    entity_pool = [pescador.Streamer(cqt_sampler, key, stash,
+                                     win_length, sample_func=sample_func)
+                   for key in stash.keys()]
+
+    return pescador.mux(entity_pool, None, working_size, lam=25)
+
+
+def create_pairwise_stream(stash, win_length, working_size=100):
+    """Return a stream of samples, with equal positive and negative
+    examples."""
+    keys = stash.keys()
+    partitions = dict()
+    # Group keys by instrument code
+    for k in keys:
+        icode = k.split("_")[0]
+        if not icode in partitions:
+            partitions[icode] = list()
+        partitions[icode].append(k)
+
+    inst_streams = []
+    for icode, key_set in partitions.items():
+        entity_pool = [pescador.Streamer(cqt_sampler, key, stash, win_length)
+                       for key in key_set]
+        stream = pescador.mux(entity_pool, n_samples=None,
+                              k=working_size, lam=20)
+        inst_streams.append(stream)
+
+    inst_streams = np.array(inst_streams)
+    triple_pool = []
+    nrange = np.arange(len(inst_streams))
+    for idx, stream in enumerate(inst_streams):
+        neg_pool = [pescador.Streamer(x)
+                    for x in inst_streams[np.not_equal(nrange, idx)]]
+        neg_stream = pescador.mux(neg_pool, n_samples=None,
+                                  k=len(neg_pool), lam=None,
+                                  with_replacement=False)
+        triples = itertools.izip(
+            inst_streams[idx], inst_streams[idx], neg_stream)
+        triple_pool.append(pescador.Streamer(triples))
+
+    cstream = pescador.mux(triple_pool, n_samples=None, k=len(triple_pool),
+                           lam=None, with_replacement=False)
+
+    return unpack_triples(cstream)
+
+
+def unpack_triples(stream):
+    for triple in stream:
+        if triple is None:
+            yield triple
+            continue
+        x1, x2, z = triple
+        yield biggie.Entity(data=x1.data,
+                            data_2=x2.data,
+                            score=float(x1.label == x2.label))
+        yield biggie.Entity(data=x1.data,
+                            data_2=z.data,
+                            score=float(x1.label == z.label))
---
+++ timbre/models.py
@@ -0,0 +1,325 @@
+import optimus
+
+TIME_DIM = 20
+OUT_DIM = 3
+GRAPH_NAME = "nlse"
+
+
+def param_init(nodes, skip_biases=True):
+    for n in nodes:
+        for k, p in n.params.items():
+            if 'bias' in k and skip_biases:
+                continue
+            optimus.random_init(p, 0, 0.1)
+
+
+def iX_c3f2_oY(n_in, n_out, size='large'):
+    # Kernel shapes
+    k0, k1, k2, k3 = dict(
+        small=(10, 20, 40, 96),
+        med=(12, 24, 48, 128),
+        large=(16, 32, 64, 192),
+        xlarge=(20, 40, 80, 256),
+        xxlarge=(24, 48, 96, 512))[size]
+
+    # Input dimensions
+    n0, n1, n2 = {
+        1: (1, 1, 1),
+        4: (3, 2, 1),
+        8: (5, 3, 2),
+        10: (3, 3, 1),
+        20: (5, 5, 1)}[n_in]
+
+    # Pool shapes
+    p0, p1, p2 = {
+        1: (1, 1, 1),
+        4: (1, 1, 1),
+        8: (1, 1, 1),
+        10: (2, 2, 1),
+        12: (2, 2, 1),
+        20: (2, 2, 2)}[n_in]
+
+    input_data = optimus.Input(
+        name='data',
+        shape=(None, 1, n_in, 192))
+
+    input_data_2 = optimus.Input(
+        name='data_2',
+        shape=(None, 1, n_in, 192))
+
+    score = optimus.Input(
+        name='score',
+        shape=(None,))
+
+    learning_rate = optimus.Input(
+        name='learning_rate',
+        shape=None)
+
+    margin = optimus.Input(
+        name='margin',
+        shape=None)
+
+    inputs = [input_data, input_data_2, score, learning_rate, margin]
+
+    # 1.2 Create Nodes
+    logscale = optimus.Log("logscale", 1.0)
+    layer0 = optimus.Conv3D(
+        name='layer0',
+        input_shape=input_data.shape,
+        weight_shape=(k0, None, n0, 13),
+        pool_shape=(p0, 2),
+        act_type='relu')
+
+    layer1 = optimus.Conv3D(
+        name='layer1',
+        input_shape=layer0.output.shape,
+        weight_shape=(k1, None, n1, 11),
+        pool_shape=(p1, 2),
+        act_type='relu')
+
+    layer2 = optimus.Conv3D(
+        name='layer2',
+        input_shape=layer1.output.shape,
+        weight_shape=(k2, None, n2, 9),
+        pool_shape=(p2, 2),
+        act_type='relu')
+
+    layer3 = optimus.Affine(
+        name='layer3',
+        input_shape=layer2.output.shape,
+        output_shape=(None, k3),
+        act_type='relu')
+
+    layer4 = optimus.Affine(
+        name='layer4',
+        input_shape=layer3.output.shape,
+        output_shape=(None, n_out),
+        act_type='linear')
+
+    param_nodes = [layer0, layer1, layer2, layer3, layer4]
+
+    # 1.1 Create cloned nodes
+    logscale_2 = logscale.clone("logscale_2")
+    layer0_2 = layer0.clone('layer0_2')
+    layer1_2 = layer1.clone('layer1_2')
+    layer2_2 = layer2.clone('layer2_2')
+    layer3_2 = layer3.clone('layer3_2')
+    layer4_2 = layer4.clone('layer4_2')
+
+    param_nodes_2 = [layer0_2, layer1_2, layer2_2, layer3_2, layer4_2]
+
+    # 1.2 Create Loss
+    # ---------------
+    #  same = y*(D^2)
+    #  diff = (1 - y) * hwr(margin - D)^2
+    #  total = ave(same_cost + diff_cost)
+
+    sqdistance = optimus.SquaredEuclidean(name='euclidean')
+    cost_sim = optimus.Product(name="cost_sim")
+
+    distance = optimus.Sqrt(name='sqrt')
+    neg_distance = optimus.Multiply(
+        name='neg_distance',
+        weight_shape=None)
+    neg_distance.weight.value = -1.0
+    margin_sum = optimus.Add(name="margin_sum", num_inputs=2)
+    hwr = optimus.RectifiedLinear(name="hwr")
+    sqhwr = optimus.Power(name='sqhwr', exponent=2.0)
+
+    pos_one = optimus.Constant(
+        name='pos_one',
+        shape=None)
+    pos_one.data.value = 1.0
+
+    neg_score = optimus.Multiply(
+        name='neg_score',
+        weight_shape=None)
+    neg_score.weight.value = -1.0
+
+    diff_selector = optimus.Add("diff_selector", num_inputs=2)
+    cost_diff = optimus.Product(name='cost_diff')
+
+    total_cost = optimus.Add('total_cost', num_inputs=2)
+    loss = optimus.Mean(name='loss')
+
+    loss_nodes = [sqdistance, distance, cost_sim, neg_distance, margin_sum,
+                  hwr, sqhwr, pos_one, neg_score, diff_selector, cost_diff,
+                  total_cost, loss]
+
+    # Graph outputs
+    total_loss = optimus.Output(name='total_loss')
+    embedding = optimus.Output(name='embedding')
+    embedding_2 = optimus.Output(name='embedding_2')
+
+    # 2. Define Edges
+    base_edges = [
+        (input_data, logscale.input),
+        (logscale.output, layer0.input),
+        (layer0.output, layer1.input),
+        (layer1.output, layer2.input),
+        (layer2.output, layer3.input),
+        (layer3.output, layer4.input),
+        (layer4.output, embedding)]
+
+    base_edges_2 = [
+        (input_data_2, logscale_2.input),
+        (logscale_2.output, layer0_2.input),
+        (layer0_2.output, layer1_2.input),
+        (layer1_2.output, layer2_2.input),
+        (layer2_2.output, layer3_2.input),
+        (layer3_2.output, layer4_2.input),
+        (layer4_2.output, embedding_2)]
+
+    trainer_edges = optimus.ConnectionManager(
+        base_edges + base_edges_2 + [
+            (layer4.output, sqdistance.input_a),
+            (layer4_2.output, sqdistance.input_b),
+            # Sim terms
+            (score, cost_sim.input_a),
+            (sqdistance.output, cost_sim.input_b),
+            (cost_sim.output, total_cost.input_0),
+            # Diff terms
+            # - margin term
+            (sqdistance.output, distance.input),
+            (distance.output, neg_distance.input),
+            (margin, margin_sum.input_0),
+            (neg_distance.output, margin_sum.input_1),
+            (margin_sum.output, hwr.input),
+            (hwr.output, sqhwr.input),
+            # - score selector
+            (score, neg_score.input),
+            (pos_one.output, diff_selector.input_0),
+            (neg_score.output, diff_selector.input_1),
+            # - product
+            (diff_selector.output, cost_diff.input_a),
+            (sqhwr.output, cost_diff.input_b),
+            (cost_diff.output, total_cost.input_1),
+            # Combined
+            (total_cost.output, loss.input),
+            (loss.output, total_loss)])
+
+    update_manager = optimus.ConnectionManager(
+        map(lambda n: (learning_rate, n.weights), param_nodes) +
+        map(lambda n: (learning_rate, n.bias), param_nodes))
+
+    param_init(param_nodes)
+
+    misc_nodes = [logscale, logscale_2]
+
+    trainer = optimus.Graph(
+        name=GRAPH_NAME,
+        inputs=inputs,
+        nodes=param_nodes + param_nodes_2 + loss_nodes + misc_nodes,
+        connections=trainer_edges.connections,
+        outputs=[total_loss, embedding, embedding_2],
+        loss=total_loss,
+        updates=update_manager.connections,
+        verbose=True)
+
+    predictor = optimus.Graph(
+        name=GRAPH_NAME,
+        inputs=[input_data],
+        nodes=param_nodes + [logscale],
+        connections=optimus.ConnectionManager(base_edges).connections,
+        outputs=[embedding],
+        verbose=True)
+
+    return trainer, predictor
+
+
+def test_pairwise(n_in):
+    input_data = optimus.Input(
+        name='data',
+        shape=(None, n_in))
+
+    input_data_2 = optimus.Input(
+        name='data_2',
+        shape=(None, n_in))
+
+    score = optimus.Input(
+        name='score',
+        shape=(None,))
+
+    margin = optimus.Input(
+        name='margin',
+        shape=None)
+
+    inputs = [input_data, input_data_2, score, margin]
+
+    # 1.2 Create Loss
+    # ---------------
+    #  same = y*D
+    #  diff = (1 - y) * hwr(margin - D)
+    #  total = ave(same_cost + diff_cost)
+
+    distance = optimus.SquaredEuclidean(name='euclidean')
+    cost_sim = optimus.Product(name="cost_sim")
+
+    neg_distance = optimus.Multiply(
+        name='neg_distance',
+        weight_shape=None)
+    neg_distance.weight.value = -1.0
+    margin_sum = optimus.Add(name="margin_sum", num_inputs=2)
+    hwr = optimus.RectifiedLinear(name="hwr")
+
+    pos_one = optimus.Constant(
+        name='pos_one',
+        shape=None)
+    pos_one.data.value = 1.0
+
+    neg_score = optimus.Multiply(
+        name='neg_score',
+        weight_shape=None)
+    neg_score.weight.value = -1.0
+
+    diff_selector = optimus.Add("diff_selector", num_inputs=2)
+    cost_diff = optimus.Product(name='cost_diff')
+
+    total_cost = optimus.Add('total_cost', num_inputs=2)
+    loss = optimus.Mean(name='loss')
+
+    loss_nodes = [distance, cost_sim, neg_distance, margin_sum, hwr,
+                  pos_one, neg_score, diff_selector, cost_diff,
+                  total_cost, loss]
+
+    # Graph outputs
+    total_loss = optimus.Output(name='total_loss')
+    dist_sim = optimus.Output(name='dist_sim')
+    dist_diff = optimus.Output(name='dist_diff')
+    dist = optimus.Output(name='dist')
+
+    # 2. Define Edges
+    trainer_edges = optimus.ConnectionManager([
+        (input_data, distance.input_a),
+        (input_data_2, distance.input_b),
+        (distance.output, dist),
+        # Sim terms
+        (score, cost_sim.input_a),
+        (distance.output, cost_sim.input_b),
+        (cost_sim.output, dist_sim),
+        (cost_sim.output, total_cost.input_0),
+        # Diff terms
+        (distance.output, neg_distance.input),
+        (margin, margin_sum.input_0),
+        (neg_distance.output, margin_sum.input_1),
+        (margin_sum.output, hwr.input),
+        (score, neg_score.input),
+        (pos_one.output, diff_selector.input_0),
+        (neg_score.output, diff_selector.input_1),
+        (diff_selector.output, cost_diff.input_a),
+        (hwr.output, cost_diff.input_b),
+        (cost_diff.output, dist_diff),
+        (cost_diff.output, total_cost.input_1),
+        # Combined
+        (total_cost.output, loss.input),
+        (loss.output, total_loss)])
+
+    trainer = optimus.Graph(
+        name=GRAPH_NAME,
+        inputs=inputs,
+        nodes=loss_nodes,
+        connections=trainer_edges.connections,
+        outputs=[total_loss, dist, dist_sim, dist_diff],
+        verbose=True)
+
+    return trainer
