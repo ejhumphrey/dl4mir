9ae9dfcecf07d89debe6b255eef32d24c5eb83b5
Eric J. Humphrey <ejhumphrey@nyu.edu>
2014-12-31 12:25:24 -0500

    PCA-LDA framework and training script

---
+++ timbre/pcalda.py
@@ -0,0 +1,119 @@
+"""write meeee"""
+import argparse
+import biggie
+import numpy as np
+import optimus
+from os import path
+
+import dl4mir.timbre.data as D
+import marl.fileutils as futil
+
+from sklearn.lda import LDA
+from sklearn.decomposition import PCA
+
+PRINT_FREQ = 50
+
+
+def fit_params(data, labels, n_components=256, n_out=3):
+    data = np.log1p(np.asarray(data))
+    shp = data.shape
+    data = data.reshape(shp[0], np.prod(shp[1:]))
+    pca = PCA(n_components=n_components, whiten=True).fit(data)
+    lda = LDA(n_components=n_out).fit(pca.transform(data), labels)
+    return {
+        'pca.bias': pca.mean_,
+        'pca.weights': pca.components_.T,
+        'lda.bias': lda.xbar_,
+        'lda.weights': lda.scalings_[:, :n_out]}
+
+
+def pca_lda_graph(n_in=20, n_components=256, n_out=3):
+    input_data = optimus.Input(
+        name='cqt',
+        shape=(None, 1, n_in, 192))
+
+    reshape = optimus.Flatten(name='flat', ndim=2)
+    logscale = optimus.Log("logscale", 1.0)
+
+    pca = optimus.CenteredAffine(
+        name='pca',
+        input_shape=(None, n_in*192),
+        output_shape=(None, n_components),
+        act_type='linear')
+
+    lda = optimus.CenteredAffine(
+        name='lda',
+        input_shape=(None, n_components),
+        output_shape=(None, n_out),
+        act_type='linear')
+
+    embedding = optimus.Output(name='embedding')
+
+    base_edges = [
+        (input_data, reshape.input),
+        (reshape.output, logscale.input),
+        (logscale.output, pca.input),
+        (pca.output, lda.input),
+        (lda.output, embedding)]
+
+    predictor = optimus.Graph(
+        name='pca-lda',
+        inputs=[input_data],
+        nodes=[logscale, reshape, pca, lda],
+        connections=optimus.ConnectionManager(base_edges).connections,
+        outputs=[embedding],
+        verbose=False)
+
+    return predictor
+
+
+def main(args):
+    predictor = pca_lda_graph(20, args.n_components, 3)
+    input_shape = list(predictor.inputs['cqt'].shape)
+    time_dim = input_shape[2]
+    input_shape[0] = args.num_points
+
+    print "Opening %s" % args.training_file
+    stash = biggie.Stash(args.training_file, cache=True)
+    stream = D.create_labeled_stream(
+        stash, time_dim, working_size=1000, threshold=0.05)
+
+    print "Starting '%s'" % args.trial_name
+    data, labels = np.zeros(input_shape), []
+    for idx, x in enumerate(stream):
+        data[idx, ...] = x.cqt
+        labels.append(x.label)
+        if len(labels) == args.num_points:
+            break
+
+    predictor.param_values = fit_params(data, labels, args.n_components, 3)
+    output_directory = futil.create_directory(args.output_directory)
+    predictor_file = path.join(output_directory, args.predictor_file)
+    param_file = predictor_file.replace(".json", ".npz")
+    optimus.save(predictor, def_file=predictor_file, param_file=param_file)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="")
+
+    # Inputs
+    parser.add_argument("training_file",
+                        metavar="training_file", type=str,
+                        help="Path to a biggie Stash file for training.")
+    parser.add_argument("num_points",
+                        metavar="num_points", type=int,
+                        help="Number to datapoints from which to fit params.")
+    parser.add_argument("n_components",
+                        metavar="n_components", type=int,
+                        help="Dimensionality of PCA-subspace.")
+    # Outputs
+    parser.add_argument("output_directory",
+                        metavar="output_directory", type=str,
+                        help="Path to save the .")
+    parser.add_argument("trial_name",
+                        metavar="trial_name", type=str,
+                        help="Unique name for this training run.")
+    parser.add_argument("predictor_file",
+                        metavar="predictor_file", type=str,
+                        help="Name for the resulting predictor graph.")
+    main(parser.parse_args())
---
+++ timbre/train_pcalda.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+#
+# Train a set of end-to-end classifiers and sweep over the checkpointed
+#    parameters to identify the early stopping point.
+#
+# Requires the following:
+#    - An environment variable `DL4MIR` has been set, pointing to the expected
+#      directory structure of data.
+#    - The script is called from the directory containing the top-level
+#      `dl4mir` source code directory.
+
+BASEDIR=${DL4MIR}/timbre_sim
+SRC=./dl4mir
+
+# Directory of optimus data files, divided by index and split, like
+#   ${BIGGIE}/${FOLD}/${SPLIT}.hdf5
+BIGGIE=${BASEDIR}/biggie
+MODELS=${BASEDIR}/models
+OUTPUTS=${BASEDIR}/outputs
+
+TRANSFORM_NAME="pca-lda"
+PARAM_TEXTLIST="paramlist.txt"
+
+
+if [ -z "$1" ] || [ -z "$2" ]; then
+    echo "Usage:"
+    echo "train.sh {config} {margin} {[0-4]|*all} {fit|select|transform|*all}"
+    echo $'\tconfig - Instrument configuration, one of {c5, c8, c12, c24}.'
+    echo $'\tmargin - Margin to use for training.'
+    echo $'\tfold# - Number of the training fold, default=all.'
+    echo $'\tphase - Name of training phase, default=all.'
+    exit 0
+fi
+
+CONFIG="$1"
+NCOMPONENTS="$2"
+NPOINTS=50000
+
+if [ -z "$3" ] || [ "$3" == "all" ];
+then
+    echo "Setting all folds"
+    FOLD_IDXS=$(seq 0 4)
+else
+    FOLD_IDXS=$3
+fi
+
+if [ -z "$4" ];
+then
+    PHASE="all"
+else
+    PHASE=$4
+fi
+
+# Fit networks
+if [ $PHASE == "all" ] || [ $PHASE == "fit" ];
+then
+    for idx in ${FOLD_IDXS}
+    do
+        python ${SRC}/timbre/pcalda.py \
+${BIGGIE}/${CONFIG}/${idx}/train.hdf5 \
+${NPOINTS} \
+${NCOMPONENTS} \
+${MODELS}/pca-lda/${NCOMPONENTS}/${CONFIG}/${idx} \
+"pca-lda" \
+${TRANSFORM_NAME}.json
+    done
+fi
+
+# Transform data
+if [ $PHASE == "all" ] || [ $PHASE == "transform" ];
+then
+    for idx in ${FOLD_IDXS}
+    do
+        for split in valid test train
+        do
+            echo "Transforming ${BIGGIE}/${CONFIG}/${idx}/${split}.hdf5"
+            python ${SRC}/common/transform_stash.py \
+${BIGGIE}/${CONFIG}/${idx}/${split}.hdf5 \
+"cqt" \
+${MODELS}/pca-lda/${NCOMPONENTS}/${CONFIG}/${idx}/${TRANSFORM_NAME}.json \
+${MODELS}/pca-lda/${NCOMPONENTS}/${CONFIG}/${idx}/${TRANSFORM_NAME}.npz \
+${OUTPUTS}/pca-lda/${NCOMPONENTS}/${CONFIG}/${idx}/${split}.hdf5
+        done
+    done
+fi
