26735434882c09bd66e768c6def5718f4e86a74f
Eric J. Humphrey <ejhumphrey@nyu.edu>
2014-12-31 12:03:32 -0500

    Quantitative eval pipeline for timbre

---
+++ timbre/evaluate.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+#
+# Train a set of end-to-end classifiers and sweep over the checkpointed
+#    parameters to identify the early stopping point.
+#
+# Requires the following:
+#    - An environment variable `DL4MIR` has been set, pointing to the expected
+#      directory structure of data.
+#    - The script is called from the directory containing the top-level
+#      `dl4mir` source code directory.
+
+BASEDIR=${DL4MIR}/timbre_sim
+SRC=./dl4mir
+
+# Directory of optimus data files, divided by index and split, like
+#   ${BIGGIE}/${FOLD}/${SPLIT}.hdf5
+MODELS=${BASEDIR}/models
+OUTPUTS=${BASEDIR}/outputs
+RESULTS=${BASEDIR}/results
+
+
+if [ -z "$1" ] || [ -z "$2" ]; then
+    echo "Usage:"
+    echo "train.sh {config} {margin} {[0-4]|*all} {fit|select|transform|*all}"
+    echo $'\tconfig - Instrument configuration, one of {c5, c8, c12, c24}.'
+    echo $'\tmargin - Margin to use for training.'
+    echo $'\tfold# - Number of the training fold, default=all.'
+    echo $'\tphase - Name of training phase, default=all.'
+    exit 0
+fi
+
+CONFIG="$1"
+MARGIN="$2"
+
+if [ -z "$3" ] || [ "$3" == "all" ];
+then
+    echo "Setting all folds"
+    FOLD_IDXS=$(seq 0 4)
+else
+    FOLD_IDXS=$3
+fi
+
+if [ -z "$4" ];
+then
+    PHASE="all"
+else
+    PHASE=$4
+fi
+
+TRIAL="${CONFIG}/${MARGIN}"
+
+# Fit networks
+if [ $PHASE == "all" ] || [ $PHASE == "classify" ];
+then
+    for idx in ${FOLD_IDXS}
+    do
+        python ${SRC}/timbre/knn_classify.py \
+${OUTPUTS}/${TRIAL}/${idx} \
+${RESULTS}/${TRIAL}/${idx}.json
+    done
+fi
--- timbre/knn_classify.py
+++ timbre/knn_classify.py
@@ -1,19 +1,25 @@
+
+import argparse
+import json
+
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import precision_recall_curve
 from sklearn.metrics import average_precision_score
 from sklearn.preprocessing import label_binarize
 from sklearn.metrics import confusion_matrix
 
+import os
 import time
-import numpy as np
 import dl4mir.timbre.data as D
+import biggie
+import marl.fileutils as futil
 
 
-def classify(train, valid, test, num_neighbors=[1, 3, 5, 9, 15, 21, 35],
+def classify(train, valid, test, num_neighbors=[1, 5, 9, 15, 21, 35, 51, 75],
              num_train=25000, num_valid=2500, num_test=25000):
-    x_train, y_train = D.sample_stash(train, num_train)[:2]
-    x_valid, y_valid = D.sample_stash(valid, num_valid)[:2]
-    x_test, y_test = D.sample_stash(test, num_test)[:2]
+    x_train, y_train = D.sample_embedding_stash(train, num_train)[:2]
+    x_valid, y_valid = D.sample_embedding_stash(valid, num_valid)[:2]
+    x_test, y_test = D.sample_embedding_stash(test, num_test)[:2]
 
     best_knn = None
     best_score = -1
@@ -38,9 +44,41 @@ def classify(train, valid, test, num_neighbors=[1, 3, 5, 9, 15, 21, 35],
     conf_mat = confusion_matrix(y_test, best_knn.predict(x_test))
     return dict(
         average_precision_micro=average_precision_micro,
-        precision_micro=precision_micro,
-        recall_micro=recall_micro,
-        confusion_matrix=conf_mat,
+        precision_micro=precision_micro.tolist(),
+        recall_micro=recall_micro.tolist(),
+        confusion_matrix=conf_mat.tolist(),
+        class_labels=best_knn.classes_.tolist(),
         train_score=best_knn.score(x_train, y_train),
         valid_score=best_knn.score(x_valid, y_valid),
         test_score=best_knn.score(x_test, y_test))
+
+
+def main(args):
+    fpath = os.path.join(args.data_directory, "{0}.hdf5")
+    train = biggie.Stash(fpath.format('train'), cache=True)
+    valid = biggie.Stash(fpath.format('valid'), cache=True)
+    test = biggie.Stash(fpath.format('test'), cache=True)
+    results = classify(train, valid, test,
+                       num_train=50000, num_valid=10000, num_test=25000)
+
+    for k in 'train', 'valid', 'test':
+        print "{0}: {1:.4}".format(k, results['{0}_score'.format(k)])
+
+    output_dir = os.path.split(args.stats_file)[0]
+    futil.create_directory(output_dir)
+    with open(args.stats_file, 'w') as fp:
+        json.dump(results, fp, indent=2)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="")
+
+    # Inputs
+    parser.add_argument("data_directory",
+                        metavar="training_file", type=str,
+                        help="Path to a biggie Stash file for training.")
+    # Outputs
+    parser.add_argument("stats_file",
+                        metavar="stats_file", type=str,
+                        help="Name for the resulting predictor graph.")
+    main(parser.parse_args())
