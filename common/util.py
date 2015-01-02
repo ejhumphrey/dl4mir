2281253810c6610b88eff98cfaf695fbd4178e93
Eric J. Humphrey <ejhumphrey@nyu.edu>
2015-01-02 11:52:16 -0500

    Moved compress samples to intervals into common module

--- common/util.py
+++ common/util.py
@@ -481,3 +481,32 @@ def slice_cqt_entity(entity, length, idx=None):
     idx = np.random.randint(entity.cqt.shape[1]) if idx is None else idx
     cqt = np.array([slice_tile(x, idx, length) for x in entity.cqt])
     return biggie.Entity(cqt=cqt, chord_label=entity.chord_labels[idx])
+
+
+def compress_samples_to_intervals(labels, time_points):
+    """Compress a set of time-aligned labels via run-length encoding.
+
+    Parameters
+    ----------
+    labels : array_like
+        Set of labels of a given type.
+    time_points : array_like
+        Points in time corresponding to the given labels.
+
+    Returns
+    -------
+    intervals : np.ndarray, shape=(N, 2)
+        Start and end times, in seconds.
+    labels : list, len=N
+        String labels corresponding to the returned intervals.
+    """
+    assert len(labels) == len(time_points)
+    intervals, new_labels = [], []
+    idx = 0
+    for label, count in run_length_encode(labels):
+        start = time_points[idx]
+        end = time_points[min([idx + count, len(labels) - 1])]
+        idx += count
+        intervals += [(start, end)]
+        new_labels += [label]
+    return np.array(intervals), new_labels
