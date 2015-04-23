eb799eba90ac1988f74cd1f27105b109227411f2
Eric J. Humphrey <ejhumphrey@nyu.edu>
2015-04-22 20:50:37 -0400

    Tiny bugfixes

--- chords/labeled_intervals_to_jamset.py
+++ chords/labeled_intervals_to_jamset.py
@@ -18,16 +18,16 @@ def main(args):
     for key, lab_files in json.load(open(args.annotation_set)).items():
         jamset[key] = pyjams.JAMS()
         for f in lab_files:
-            intervals, labels = mir_eval.io.load_labeled_intervals(f)
+            intervals, labels = mir_eval.io.load_labeled_intervals(str(f))
             annot = jamset[key].chord.create_annotation()
             pyjams.util.fill_range_annotation_data(
                 intervals[:, 0], intervals[:, 1], labels, annot)
 
-            annot.annotation_metadata.update(metadata.get(key, {}))
+            annot.annotation_metadata.update(**metadata.get(key, {}))
             annot.sandbox.source_file = f
             annot.sandbox.timestamp = time.asctime()
 
-    futil.create_directory(os.path.split(args.output_file))
+    futil.create_directory(os.path.split(args.output_file)[0])
     util.save_jamset(jamset, args.output_file)
 
 
