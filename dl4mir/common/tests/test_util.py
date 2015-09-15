660fe1e889f42d8a214ed6befd756ae41fd46d6d
Eric J. Humphrey <ejhumphrey@nyu.edu>
2015-09-14 22:54:57 -0400

    Moved scripts and corresponding tests to experiments dir

--- dl4mir/common/tests/test_util.py
+++ dl4mir/common/tests/test_util.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import optimus
 import dl4mir.common.util as U
 
 
@@ -37,3 +37,27 @@ def test_stratify():
             for b, y in split.items():
                 if a != b:
                     assert not set(x).intersection(set(y))
+
+
+def test_convolve():
+    input_data = optimus.Input(name='x_in', shape=(None, 1, 1, 1))
+    flatten = optimus.Flatten(name='flatten', ndim=1)
+    output_data = optimus.Output(name='x_out')
+    edges = optimus.ConnectionManager([
+        (input_data, flatten.input),
+        (flatten.output, output_data)])
+
+    transform = optimus.Graph(
+        name='test',
+        inputs=[input_data],
+        nodes=[flatten],
+        connections=edges.connections,
+        outputs=[output_data])
+
+    x = np.arange(10).reshape(1, 10, 1)
+    y = np.array(['a', 'b'])
+    entity = biggie.Entity(x_in=x, y=y)
+    z = util.convolve(entity, transform, 'x_in')
+
+    np.testing.assert_equal(z.x_out, np.arange(10))
+    np.testing.assert_equal(z.y, y)
